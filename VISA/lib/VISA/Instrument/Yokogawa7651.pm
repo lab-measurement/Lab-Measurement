e459dfdb45093b44cfa5faadaef53e486f6c8885
Daniel Schröer <daniel@daimla1.de>
2005-01-30 20:37:17 +0000

    tabs in spaces konvertiert f�r pod
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@38 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- VISA/lib/VISA/Instrument/Yokogawa7651.pm
+++ VISA/lib/VISA/Instrument/Yokogawa7651.pm
@@ -10,144 +10,144 @@ our $VERSION = sprintf("%d.%02d", q$Revision$ =~ /(\d+)\.(\d+)/);
 our @ISA=('VISA::Instrument::SafeSource');
 
 sub new {
-	my $proto = shift;
+    my $proto = shift;
     my $class = ref($proto) || $proto;
     my $self = new VISA::Instrument::SafeSource();
     bless ($self, $class);
 
-	$self->{vi}=new VISA::Instrument(@_);
-	
-	return $self
+    $self->{vi}=new VISA::Instrument(@_);
+    
+    return $self
 }
 
 sub _set_voltage {
-	my $self=shift;
-	my $voltage=shift;
-	$self->_set($voltage);
+    my $self=shift;
+    my $voltage=shift;
+    $self->_set($voltage);
 }
 
 sub set_current {
-	my $self=shift;
-	my $voltage=shift;
-	$self->_set($voltage);
+    my $self=shift;
+    my $voltage=shift;
+    $self->_set($voltage);
 }
 
 sub _set {
-	my $self=shift;
-	my $value=shift;
-	my $cmd=sprintf("S%e",$value);
-	$self->{vi}->Write($cmd);
-	$cmd="E";
-	$self->{vi}->Write($cmd);
+    my $self=shift;
+    my $value=shift;
+    my $cmd=sprintf("S%e",$value);
+    $self->{vi}->Write($cmd);
+    $cmd="E";
+    $self->{vi}->Write($cmd);
 }
 
 sub get_voltage {
-	my $self=shift;
-	return $self->_get();
+    my $self=shift;
+    return $self->_get();
 }
 
 sub get_current {
-	my $self=shift;
-	return $self->_get();
+    my $self=shift;
+    return $self->_get();
 }
 
 sub _get {
-	my $self=shift;
-	my $cmd="OD";
-	my $result=$self->{vi}->Query($cmd);
-	$result=~/....([\+\-\d\.E]*)/;
-	return $1;
+    my $self=shift;
+    my $cmd="OD";
+    my $result=$self->{vi}->Query($cmd);
+    $result=~/....([\+\-\d\.E]*)/;
+    return $1;
 }
 
 sub set_current_mode {
-	my $self=shift;
-	my $cmd="F5";
-	$self->{vi}->Write($cmd);
+    my $self=shift;
+    my $cmd="F5";
+    $self->{vi}->Write($cmd);
 }
 
 sub set_voltage_mode {
-	my $self=shift;
-	my $cmd="F1";
-	$self->{vi}->Write($cmd);
+    my $self=shift;
+    my $cmd="F1";
+    $self->{vi}->Write($cmd);
 }
 
 sub set_range {
-	my $self=shift;
-	my $range=shift;
-	my $cmd="R$range";
-	  #fixed voltage mode
-	  #	2	10mV
-	  # 3	100mV
-	  # 4	1V
-	  # 5	10V
-	  # 6	30V
-	  #fixed current mode
-	  # 4	1mA
-	  # 5	10mA
-	  # 6	100mA
-	$self->{vi}->Write($cmd);
+    my $self=shift;
+    my $range=shift;
+    my $cmd="R$range";
+      #fixed voltage mode
+      # 2   10mV
+      # 3   100mV
+      # 4   1V
+      # 5   10V
+      # 6   30V
+      #fixed current mode
+      # 4   1mA
+      # 5   10mA
+      # 6   100mA
+    $self->{vi}->Write($cmd);
 }
 
 sub get_info {
-	my $self=shift;
-	my $result=$self->{vi}->Query("OS");
-	return $result;
+    my $self=shift;
+    my $result=$self->{vi}->Query("OS");
+    return $result;
 }
 
 sub output_on {
-	my $self=shift;
-	$self->{vi}->Write('O1');
-	$self->{vi}->Write('E');
+    my $self=shift;
+    $self->{vi}->Write('O1');
+    $self->{vi}->Write('E');
 }
-	
+    
 sub output_off {
-	my $self=shift;
-	$self->{vi}->Write('O0');
-	$self->{vi}->Write('E');
+    my $self=shift;
+    $self->{vi}->Write('O0');
+    $self->{vi}->Write('E');
 }
 
 sub get_output {
-	my $self=shift;
-	my %res=$self->get_status();
-	return $res{output};
+    my $self=shift;
+    my %res=$self->get_status();
+    return $res{output};
 }
 
 sub initialize {
-	my $self=shift;
-	$self->{vi}->Write('RC');
+    my $self=shift;
+    $self->{vi}->Write('RC');
 }
 
 sub set_voltage_limit {
-	my $self=shift;
-	my $value=shift;
-	my $cmd=sprintf("LV%e",$value);
-	$self->{vi}->Write($cmd);
+    my $self=shift;
+    my $value=shift;
+    my $cmd=sprintf("LV%e",$value);
+    $self->{vi}->Write($cmd);
 }
 
 sub set_current_limit {
-	my $self=shift;
-	my $value=shift;
-	my $cmd=sprintf("LA%e",$value);
-	$self->{vi}->Write($cmd);
+    my $self=shift;
+    my $value=shift;
+    my $cmd=sprintf("LA%e",$value);
+    $self->{vi}->Write($cmd);
 }
 
 sub get_status {
-	my $self=shift;
-	my $status=$self->{vi}->Query('OC');
-	
-	$status=~/STS1=(\d*)/;
-	$status=$1;
-	my @flags=qw/
-		CAL_switch	memory_card	calibration_mode	output
-		unstable	error	execution	setting/;
-	my %result;
-	for (0..7) {
-		if ($status&128) {
-			$result{$flags[$_]}=1;
-		}
-		$status<<=1;
-	}
-	return %result;
+    my $self=shift;
+    my $status=$self->{vi}->Query('OC');
+    
+    $status=~/STS1=(\d*)/;
+    $status=$1;
+    my @flags=qw/
+        CAL_switch  memory_card calibration_mode    output
+        unstable    error   execution   setting/;
+    my %result;
+    for (0..7) {
+        if ($status&128) {
+            $result{$flags[$_]}=1;
+        }
+        $status<<=1;
+    }
+    return %result;
 }
 
 1;
@@ -161,9 +161,9 @@ VISA::Instrument::Yokogawa7651 - a Yokogawa 7651 DC source
     use VISA::Instrument::Yokogawa7651;
     
     my $gate14=new VISA::Instrument::Yokogawa7651(0,11);
-	$gate14->set_range(5);
-	$gate14->set_voltage(0.745);
-	print $gate14->get_voltage();
+    $gate14->set_range(5);
+    $gate14->set_voltage(0.745);
+    print $gate14->get_voltage();
 
 =head1 DESCRIPTION
 
@@ -179,17 +179,17 @@ VISA::Instrument::Yokogawa7651 - a Yokogawa 7651 DC source
 
 =head2 set_range($range)
 
-	Fixed voltage mode
-		2	10mV
-		3	100mV
-		4	1V
-		5	10V
-		6	30V
+    Fixed voltage mode
+        2   10mV
+        3   100mV
+        4   1V
+        5   10V
+        6   30V
 
-	Fixed current mode
-		4	1mA
-		5	10mA
-		6	100mA
+    Fixed current mode
+        4   1mA
+        5   10mA
+        6   100mA
 
 =head2 get_info()
 
@@ -207,7 +207,7 @@ VISA::Instrument::Yokogawa7651 - a Yokogawa 7651 DC source
 
 =head2 get_status()
 
-=head1 SPECIFICATIONS
+=head1 INSTRUMENT SPECIFICATIONS
 
 =head2 DC voltage
 
@@ -215,28 +215,28 @@ The stability (24h) is the value at 23 ± 1°C. The stability (90days),
 accuracy (90days) and accuracy (1year) are values at 23 ± 5°C.
 The temperature coefficient is the value at 5 to 18°C and 28 to 40°C.
 
- Range	Maximum		Resolution	Stability 24h	Stability 90d	
-		Output					±(% of setting	±(% of setting	
-                                +µV)            +µV)			
+ Range  Maximum     Resolution  Stability 24h   Stability 90d   
+        Output                  ±(% of setting  ±(% of setting  
+                                +µV)            +µV)            
  ------------------------------------------------------------- 
- 10mV	±12.0000mV	100nV		0.002 + 3		0.014 + 4		
- 100mV	±120.000mV	1µV			0.003 + 3		0.014 + 5		
- 1V		±1.20000V	10µV		0.001 + 10		0.008 + 50		
- 10V	±12.0000V	100µV		0.001 + 20		0.008 + 100		
- 30V	±32.000V	1mV			0.001 + 50		0.008 + 200		
+ 10mV   ±12.0000mV  100nV       0.002 + 3       0.014 + 4       
+ 100mV  ±120.000mV  1µV         0.003 + 3       0.014 + 5       
+ 1V     ±1.20000V   10µV        0.001 + 10      0.008 + 50      
+ 10V    ±12.0000V   100µV       0.001 + 20      0.008 + 100     
+ 30V    ±32.000V    1mV         0.001 + 50      0.008 + 200     
 
 
 
- Range	Accuracy 90d	Accuracy 1yr	Temperature
-		±(% of setting	±(% of setting	Coefficient
-		+µV)			+µV)			±(% of setting
-										+µV)/°C
+ Range  Accuracy 90d    Accuracy 1yr    Temperature
+        ±(% of setting  ±(% of setting  Coefficient
+        +µV)            +µV)            ±(% of setting
+                                        +µV)/°C
  -----------------------------------------------------
- 10mV	0.018 + 4		0.025 + 5		0.0018 + 0.7
- 100mV	0.018 + 10		0.025 + 10		0.0018 + 0.7
- 1V		0.01 + 100		0.016 + 120		0.0009 + 7
- 10V	0.01 + 200		0.016 + 240		0.0008 + 10
- 30V	0.01 + 500		0.016 + 600		0.0008 + 30
+ 10mV   0.018 + 4       0.025 + 5       0.0018 + 0.7
+ 100mV  0.018 + 10      0.025 + 10      0.0018 + 0.7
+ 1V     0.01 + 100      0.016 + 120     0.0009 + 7
+ 10V    0.01 + 200      0.016 + 240     0.0008 + 10
+ 30V    0.01 + 500      0.016 + 600     0.0008 + 30
  
 
  
