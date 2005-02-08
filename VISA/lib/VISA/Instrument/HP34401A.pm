48df4b28f89463bec6215584278445b5c8138275
Daniel Schröer <daniel@daimla1.de>
2005-02-08 17:38:28 +0000

    Etliche neue Methoden und zugeh�rige Tests.
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@47 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- VISA/lib/VISA/Instrument/HP34401A.pm
+++ VISA/lib/VISA/Instrument/HP34401A.pm
@@ -20,12 +20,96 @@ sub new {
 
 sub read_voltage_dc {
 	my $self=shift;
-	my $range=shift;
-	my $resolution=shift;
+	my ($range,$resolution)=@_;
+	
+	$range="DEF" unless (defined $range);
+	$resolution="DEF" unless (defined $resolution);
 	
 	my $cmd=sprintf("MEASure:VOLTage:DC? %u,%f",$range,$resolution);
 	my ($value)=split "\n",$self->{vi}->Query($cmd);
 	return $value;
+}
+
+sub read_voltage_ac {
+	my $self=shift;
+	my ($range,$resolution)=@_;
+	
+	$range="DEF" unless (defined $range);
+	$resolution="DEF" unless (defined $resolution);
+	
+	my $cmd=sprintf("MEASure:VOLTage:AC? %u,%f",$range,$resolution);
+	my ($value)=split "\n",$self->{vi}->Query($cmd);
+	return $value;
+}
+
+sub read_current_dc {
+	my $self=shift;
+	my ($range,$resolution)=@_;
+	
+	$range="DEF" unless (defined $range);
+	$resolution="DEF" unless (defined $resolution);
+	
+	my $cmd=sprintf("MEASure:CURRent:DC? %u,%f",$range,$resolution);
+	my ($value)=split "\n",$self->{vi}->Query($cmd);
+	return $value;
+}
+
+sub read_current_ac {
+	my $self=shift;
+	my ($range,$resolution)=@_;
+	
+	$range="DEF" unless (defined $range);
+	$resolution="DEF" unless (defined $resolution);
+	
+	my $cmd=sprintf("MEASure:CURRent:AC? %u,%f",$range,$resolution);
+	my ($value)=split "\n",$self->{vi}->Query($cmd);
+	return $value;
+}
+
+sub display_text {
+	my $self=shift;
+	my $text=shift;
+	
+	if ($text) {
+		$self->{vi}->Write(qq(DISPlay:TEXT "$text"));
+	} else {
+		chomp($text=$self->{vi}->Query(qq(DISPlay:TEXT?)));
+		$text=~s/\"//g;
+	}
+	return $text;
+}
+
+sub display_on {
+	my $self=shift;
+	$self->{vi}->Write("DISPlay ON");
+}
+
+sub display_off {
+	my $self=shift;
+	$self->{vi}->Write("DISPlay OFF");
+}
+
+sub display_clear {
+	my $self=shift;
+	$self->{vi}->Write("DISPlay:TEXT:CLEar");
+}
+
+sub beep {
+	my $self=shift;
+	$self->{vi}->Write("SYSTem:BEEPer");
+}
+
+sub get_error {
+	my $self=shift;
+	chomp(my $err=$self->{vi}->Query("SYSTem:ERRor?"));
+	my ($err_num,$err_msg)=split ",",$err;
+	$err_msg=~s/\"//g;
+	return ($err_num,$err_msg);
+}
+
+sub reset {
+	my $self=shift;
+	$self->{vi}->Write("*RST");
 }
 
 1;
@@ -38,18 +122,105 @@ VISA::Instrument::HP34401A - a HP 34401A digital multimeter
 
     use VISA::Instrument::HP34401A;
     
-    my $hp22=new VISA::Instrument::HP34401A(0,22);
-	print $hp22->read_voltage_dc(10,0.00001);
+    my $hp=new VISA::Instrument::HP34401A(0,22);
+    print $hp->read_voltage_dc(10,0.00001);
 
 =head1 DESCRIPTION
 
-=head1 CONSTRUCTORS
+=head1 CONSTRUCTOR
 
-=head2 new($gpib_board,$gpib_addr)
+    my $hp=new(\%options);
 
 =head1 METHODS
 
-=head2 read_voltage_dc($range,$resolution);
+=head2 read_voltage_dc
+
+    $datum=$hp->read_voltage_dc($range,$resolution);
+
+Preset and make a dc voltage measurement with the specified range
+and resolution.
+
+=over 4
+
+=item $range
+
+Range is given in terms of volts and can be [0.1|1|10|100|1000|MIN|MAX|DEF]. DEF is default.
+
+=item $resolution
+
+Resolution is given in terms of $range or [MIN|MAX|DEF]. $resolution=0.0001 means 4½ digits for example.
+The best resolution is 100nV: $range=0.1;$resolution=0.000001.
+
+=back
+
+=head2 read_voltage_ac
+
+    $datum=$hp->read_voltage_ac($range,$resolution);
+
+Preset and make an ac voltage measurement with the specified range
+and resolution. For ac measurements, resolution is actually fixed
+at 6½ digits. The resolution parameter only affects the front-panel display.
+
+=head2 read_current_dc
+
+    $datum=$hp->read_current_dc($range,$resolution);
+
+Preset and make a dc current measurement with the specified range
+and resolution.
+
+=head2 read_current_ac
+
+    $datum=$hp->read_current_ac($range,$resolution);
+
+Preset and make an ac current measurement with the specified range
+and resolution. For ac measurements, resolution is actually fixed
+at 6½ digits. The resolution parameter only affects the front-panel display.
+
+=head2 display_on
+
+    $hp->display_on();
+
+Turn the front-panel display on.
+
+=head2 display_off
+
+    $hp->display_off();
+
+Turn the front-panel display off.
+
+=head2 display_text
+
+    $hp->display_text($text);
+    print $hp->display_text();
+
+Display a message on the front panel. The multimeter will display up to 12
+characters in a message; any additional characters are truncated.
+Without parameter the displayed message is returned.
+
+=head2 display_clear
+
+    $hp->display_clear();
+
+Clear the message displayed on the front panel.
+
+=head2 beep
+
+    $hp->beep();
+
+Issue a single beep immediately.
+
+=head2 get_error
+
+    ($err_num,$err_msg)=$hp->get_error();
+
+Query the multimeter's error queue. Up to 20 errors can be stored in the
+queue. Errors are retrieved in first-in-first out (FIFO) order.
+
+=head2 reset
+
+    $hp->reset();
+
+Reset the multimeter to its power-on configuration.
 
 =head1 CAVEATS/BUGS
 
--- VISA/t/5_hp34401a.t
+++ VISA/t/5_hp34401a.t
@@ -2,10 +2,19 @@
 #$Id$
 
 use strict;
-use Test::More tests => 3;
+use Test::More tests => 10;
 
 BEGIN { use_ok('VISA::Instrument::HP34401A') };
 
-ok(my $hp=new VISA::Instrument::HP34401A(0,24),'Open any HP');
+ok(my $hp=new VISA::Instrument::HP34401A(0,24),'Open HP 24');
 ok(my $voltage=$hp->read_voltage_dc(10,0.00001),'read_voltage_dc(10,0.00001)');
 diag "read $voltage";
+ok($hp->display_text("japh"),'display_text($text)');
+ok("japh" eq $hp->display_text(),'display_text()');
+ok($hp->display_off(),'display_off');
+ok($hp->display_on(),'display_on');
+ok($hp->display_clear(),'display_clear');
+ok(my ($err_num,$err_msg)=$hp->get_error(),'get_error');
+diag "error $err_num: $err_msg";
+ok($hp->beep(),"beep");
+
