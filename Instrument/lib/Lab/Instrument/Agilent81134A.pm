b5049fb3219f87e9273ea2f59429d891df3437c4
Daniel Schröer <daniel@daimla1.de>
2005-12-02 15:49:43 +0000

    zwei Beispiele aus Userguide eingebaut (ungetestet)
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@218 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- Instrument/lib/Lab/Instrument/Agilent81134A.pm
+++ Instrument/lib/Lab/Instrument/Agilent81134A.pm
@@ -18,6 +18,67 @@ sub new {
     return $self
 }
 
+sub clock_example {
+    my $self=shift;
+    
+    my @cmd=(
+    #Protect the DUT
+        ':OUTP:CENT OFF',       #disconnect channels
+
+    #Set up the Instrument
+        ':FUNC PATT',           #set mode to Pulse/Pattern
+        ':FREQ 200 MHz',        #set freq to 200 MHz
+
+    #Set up Channel 1
+        ':FUNC:MODE1 SQU',      #set pattern mode to Square
+        ':VOLT1:HIGH 1.000 V',  #set high-Level to 1 V
+        ':VOLT1:LOW 0 V',       #set low-level to 0 V
+        ':OUTP1:POS ON',        #enable output channel 1
+
+    #Set up Channel 2
+        ':FUNC:MODE2 SQU',      #set pattern mode to Square
+        ':OUTP2:DIV 2',         #set freq div to 2
+        ':VOLT2:HIGH 1.000 V',  #set the high-Level to 1 V
+        ':VOLT2:LOW 0 V',       #set low-level to 0 V
+        ':OUTP2:POS ON',        #enable output channel 2
+
+    #Generate the Signals
+        ':OUTP:CENT ON',        #reconnect the channels
+        ':OUTP0 ON',            #enable trigger output
+    );
+    for (@cmd) {
+        $self->{vi}->Write($_);
+    }
+}
+
+sub pulse_example {
+    my $self=shift;
+    
+    my @cmd=(
+    #Protect the DUT
+        ':OUTP:CENT OFF',       #disconnect channels
+
+    #Set up the Instrument
+        ':FUNC PATT',           #set mode to Pulse/Pattern
+        ':PER 20 ns',           #set period to 20 ns
+
+    #Set up Channel 1
+        ':FUNC:MODE1 PULSE',    #set pattern mode to Pulse
+        ':WIDT1 5 ns',          #set width to 5 ns
+        ':VOLT1:AMPL 2.000 V',  #set ampl to 2 V
+        ':VOLT1:OFFSET 1.5 V',  #set offset to 1.5 V
+        ':OUTP1:POS ON',        #enable output channel 1
+
+    #Generate the Signals
+        ':OUTP:CENT ON',        #reconnect the channels
+        ':OUTP0:SOUR PER',      #use trigger mode Pulse
+        ':OUTP0 ON',            #enable trigger output
+    );
+    for (@cmd) {
+        $self->{vi}->Write($_);
+    }
+}
+
 1;
 
 =head1 NAME
