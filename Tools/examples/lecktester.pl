e105ff2ab5587e80ed150b581ee7a9796f7fe38b
Daniel Schröer <daniel@daimla1.de>
2006-03-21 18:42:25 +0000

    bessere Lecktesterversion, die allerdings auf Haribo nicht geht
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@285 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- Tools/examples/lecktester.pl
+++ Tools/examples/lecktester.pl
@@ -14,8 +14,7 @@ my ($gpib,$bereich,$filename,$comment)=@ARGV;
 
 my $hp=new Lab::Instrument::HP34401A({GPIB_board=>0,GPIB_address=>$gpib});
 
-open GP,">$filename.gnuplot" or die "cannot open gnuplot file";
-print GP <<GNUPLOT;
+my $gp1=<<GNUPLOT1;
 set xdata time
 set timefmt "%Y-%m-%d %H:%M:%S"
 set format x "%H:%M:%S"
@@ -24,15 +23,22 @@ set xlabel "Time"
 set xtics rotate
 set title "$comment"
 plot "$filename" using 1:3 with lines
-pause 5
+GNUPLOT1
+my $gp2=<<GNUPLOT2;
+replot
+pause 3
 reread
-GNUPLOT
+GNUPLOT2
+open GP,">$filename.gnuplot" or die "cannot open gnuplot file";
+print GP $gp1,$gp2;
 close GP;
 
 open LOG,">$filename" or die "cannot open log file";
 my $old_fh = select(LOG);
 $| = 1;
 select($old_fh);
+
+print LOG "#$comment\n";
 
 #nur damit gnuplot kein leeres file findet
     my $read_volt=$hp->read_voltage_dc(10,0.0001);
@@ -45,20 +51,28 @@ select($old_fh);
 
 system("gnuplot $filename.gnuplot &");
 
-print "Leak test in progress\nPress 's' to stop.";
-print LOG "#$comment\n";
+print "Leak test in progress\nPress 's' to stop; 'm' to mark position.\n";
 
 my $key;
 ReadMode('cbreak');
-while (ReadKey(-1) ne "s") {
+while ($key ne "s") {
     my $read_volt=$hp->read_voltage_dc(10,0.0001);
     $read_volt=12 if ($read_volt > 12);
     my $rate=($read_volt/10)*$bereich;
     my ($s,$ms)=gettimeofday();
     my ($sec,$min,$hour,$mday,$mon,$year)=localtime($s);
     $year+=1900;$mon++;
-    printf LOG "%4d-%02d-%02d %02d:%02d:%.2f\t%f\n",$year,$mon,$mday,$hour,$min,$sec+$ms/1e6,$rate;
+    my $timestamp=sprintf "%4d-%02d-%02d %02d:%02d:%.2f\t%f",$year,$mon,$mday,$hour,$min,$sec+$ms/1e6,$rate;
+    print LOG "$timestamp\n";
+    if ($key eq "m") {
+        print "Marking position $timestamp\n";
+        $gp1.=qq(set arrow from "$timestamp", graph 0 to "$timestamp", graph 1 nohead\n);
+        open GP,">$filename.gnuplot" or die "cannot open gnuplot file";
+        print GP $gp1,$gp2;
+        close GP;
+    }
     usleep(500000);
+    $key=ReadKey(-1);
 }
 ReadMode('normal');
 close LOG;
