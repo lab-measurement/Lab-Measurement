28ae09efb79516db3350c3c43bd0d0a9414bf281
Daniel Schröer <daniel@daimla1.de>
2006-03-21 19:10:20 +0000

    jetzt mit Pipes == viel cooler!
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@286 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- Tools/examples/lecktester.pl
+++ Tools/examples/lecktester.pl
@@ -1,7 +1,7 @@
 #!/usr/bin/perl
 
 use strict;
-use Lab::Instrument::HP34401A;
+#use Lab::Instrument::HP34401A;
 use Time::HiRes qw/usleep gettimeofday/;
 use Term::ReadKey;
 
@@ -12,7 +12,7 @@ unless (@ARGV == 4) {
 }
 my ($gpib,$bereich,$filename,$comment)=@ARGV;
 
-my $hp=new Lab::Instrument::HP34401A({GPIB_board=>0,GPIB_address=>$gpib});
+#my $hp=new Lab::Instrument::HP34401A({GPIB_board=>0,GPIB_address=>$gpib});
 
 my $gp1=<<GNUPLOT1;
 set xdata time
@@ -22,16 +22,13 @@ set ylabel "Leak rate (10^{-9} mbar l s^{-1})"
 set xlabel "Time"
 set xtics rotate
 set title "$comment"
-plot "$filename" using 1:3 with lines
 GNUPLOT1
 my $gp2=<<GNUPLOT2;
-replot
-pause 3
-reread
+plot "$filename" using 1:3 with lines
 GNUPLOT2
-open GP,">$filename.gnuplot" or die "cannot open gnuplot file";
-print GP $gp1,$gp2;
-close GP;
+
+my $gpipe=get_pipe() or die;
+print $gpipe $gp1;
 
 open LOG,">$filename" or die "cannot open log file";
 my $old_fh = select(LOG);
@@ -40,39 +37,53 @@ select($old_fh);
 
 print LOG "#$comment\n";
 
-#nur damit gnuplot kein leeres file findet
-    my $read_volt=$hp->read_voltage_dc(10,0.0001);
-    $read_volt=12 if ($read_volt > 12);
-    my $rate=($read_volt/10)*$bereich;
-    my ($s,$ms)=gettimeofday();
-    my ($sec,$min,$hour,$mday,$mon,$year)=localtime($s);
-    $year+=1900;$mon++;
-    printf LOG "%4d-%02d-%02d %02d:%02d:%.2f\t%f\n",$year,$mon,$mday,$hour,$min,$sec+$ms/1e6,$rate;
-
-system("gnuplot $filename.gnuplot &");
-
 print "Leak test in progress\nPress 's' to stop; 'm' to mark position.\n";
 
 my $key;
 ReadMode('cbreak');
 while ($key ne "s") {
-    my $read_volt=$hp->read_voltage_dc(10,0.0001);
+    my $read_volt=rand(10);#$hp->read_voltage_dc(10,0.0001);
     $read_volt=12 if ($read_volt > 12);
     my $rate=($read_volt/10)*$bereich;
     my ($s,$ms)=gettimeofday();
     my ($sec,$min,$hour,$mday,$mon,$year)=localtime($s);
     $year+=1900;$mon++;
-    my $timestamp=sprintf "%4d-%02d-%02d %02d:%02d:%.2f\t%f",$year,$mon,$mday,$hour,$min,$sec+$ms/1e6,$rate;
-    print LOG "$timestamp\n";
+    my $timestamp=sprintf "%4d-%02d-%02d %02d:%02d:%.2f",$year,$mon,$mday,$hour,$min,$sec+$ms/1e6;
+    print LOG "$timestamp\t$rate\n";
     if ($key eq "m") {
         print "Marking position $timestamp\n";
-        $gp1.=qq(set arrow from "$timestamp", graph 0 to "$timestamp", graph 1 nohead\n);
-        open GP,">$filename.gnuplot" or die "cannot open gnuplot file";
-        print GP $gp1,$gp2;
-        close GP;
+        my $mark=qq(set arrow from "$timestamp", graph 0 to "$timestamp", graph 1 nohead lt 2 lw 2\n);
+        print $gpipe $mark;
+        $gp1.=$mark;
     }
     usleep(500000);
+    print $gpipe $gp2;
     $key=ReadKey(-1);
 }
 ReadMode('normal');
 close LOG;
+close $gpipe;
+
+open GP,">$filename.gnuplot" or die "cannot open gnuplot file";
+print GP $gp1,<<GNUPLOT3,$gp2;
+set term post col enh
+set out "$filename.ps"
+GNUPLOT3
+
+system("gnuplot $filename.gnuplot");
+
+sub get_pipe {
+	my $gpname;
+	if ($^O =~ /MSWin32/) {
+		$gpname="pgnuplot";
+	} else {
+		$gpname="gnuplot";
+	}
+	if (open my $GP,"| $gpname") {
+		my $oldfh = select($GP);
+		$| = 1;
+		select($oldfh);
+		return $GP;
+	}
+	return undef;
+}
