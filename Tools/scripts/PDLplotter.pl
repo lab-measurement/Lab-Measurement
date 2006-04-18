69826248f35b0c743acfddfc00414844fadf5bd0
Daniel Schröer <daniel@daimla1.de>
2006-04-18 14:47:30 +0000

    Teil2
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@357 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- Tools/scripts/PDLplotter.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use strict;
-use TSKTools;
-use PDL;
-use PDL::Graphics::TriD;
-
-my $a=TSKTools::TSKload('lade_rawdata/lade_');
-imag3d[$a->slice("(0),:,:"),$a->slice("(1),:,:"),$a->slice("(2),:,:")],{Lines=>0};
