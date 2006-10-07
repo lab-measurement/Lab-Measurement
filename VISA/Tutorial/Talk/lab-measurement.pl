7ce42121844546ddf89c94a0336b99989ca60f8a
Daniel Schröer <daniel@daimla1.de>
2006-10-07 13:23:19 +0000

    Geht jetzt ganz gut.
    Widerstand bei Diode nicht ungedingt sinnvoll. Lasse es trotzdem.
    
    
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@522 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- VISA/Tutorial/Talk/lab-measurement.pl
+++ VISA/Tutorial/Talk/lab-measurement.pl
@@ -6,32 +6,49 @@ use Lab::Instrument::HP34401A;
 use Time::HiRes qw/usleep/;
 use Lab::Measurement;
 
+################################
+
+my $start_voltage   =  1.5;
+my $end_voltage     = -3.5;
+my $step            = -0.1;
+
+my $knick_gpib      = 14;
+my $hp_gpib         = 23;
+
+my $sample          = "Zenerdiode";
+my $title           = "Messung mit Lab::Measurement";
+my $comment         = <<COMMENT;
+Reihenschaltung aus Widerstand 1kOhm und Zenerdiode.
+COMMENT
+
+################################
+
 my $knick=new Lab::Instrument::KnickS252({
     'GPIB_board'    => 0,
-    'GPIB_address'  => 14,
+    'GPIB_address'  => $knick_gpib,
     'gate_protect'  => 0,
 });
 
-my $hp=new Lab::Instrument::HP34401A(0,23);
+my $hp=new Lab::Instrument::HP34401A(0,$hp_gpib);
 
 my $measurement=new Lab::Measurement(
-    sample          => "Zenerdiode",
-    title           => "Messung mit Lab::Measurement",
+    sample          => $sample,
+    title           => $title,
     filename_base   => 'zener_kennlinie',
-    description     => "Reihenschaltung aus Widerstand 1kOhm und Zenerdiode."
+    description     => $comment,
 
-    live_plot       => 'current',
+    live_plot       => 'diode current',
 
     constants       => [
         {
-            'name'          => 'R_Vorwiderstand',
+            'name'          => 'R',
             'value'         => '1000',
         },
     ],
     columns         => [
         {
             'unit'          => 'V',
-            'label'         => 'V_{Bias}',
+            'label'         => 'V_{bias}',
             'description'   => 'Bias Voltage',
         },
         {
@@ -44,49 +61,59 @@ my $measurement=new Lab::Measurement(
         {
             'unit'          => 'V',
             'expression'    => '$C0',
-            'label'         => 'V_{Bias}',
-            'min'           => -3.5,
-            'max'           =>  1.5,
+            'label'         => 'V_{bias}',
             'description'   => 'Bias voltage',
-        },
-        {
-            'unit'          => 'V',
-            'expression'    => '$C1',
-            'label'         => 'V_{Resistor}',
-            'description'   => 'Voltage drop of serial resistor',
+            'min'           => ($start_voltage < $end_voltage)
+                               ? $start_voltage
+                               : $end_voltage,
+            'max'           => ($start_voltage < $end_voltage)
+                               ? $end_voltage
+                               : $start_voltage,
         },
         {
             'unit'          => 'mA',
-            'expression'    => '1000*($C1/R_Vorwiderstand)',
-            'label'         => 'I_{Resistor}',
-            'description'   => 'Current through resistor',
+            'expression'    => '1000*($C1/R)',
+            'label'         => 'I_{diode}',
+            'description'   => 'Current through diode',
+            'min'           => '-1.2',
+            'max'           => '1.2',
+        },
+        {
+            'unit'          => 'Ohm',
+            'expression'    => 'R * ($C0/$C1 - 1)',
+            'label'         => 'R_{diode}',
+            'description'   => 'Diode resistance',
         },
     ],
     plots           => {
-        'voltage drop'    => {
+        'diode current'    => {
             'type'          => 'line',
             'xaxis'         => 0,
             'yaxis'         => 1,
             'grid'          => 'xtics ytics',
         },
-        'current'    => {
+        'diode resistance' => {
             'type'          => 'line',
             'xaxis'         => 0,
             'yaxis'         => 2,
             'grid'          => 'xtics ytics',
+            'logscale'      => 'y',
         },
     },
 );
 
 $measurement->start_block();
 
-my $stepsign=$step/abs($step);
-for (my $volt=1.5 ; $volt>=-3.5 ; $volt+=-0.1) {
+for (
+    my $volt = $start_voltage;
+    ($volt - $end_voltage) / $step < 0.5;
+    $volt += $step
+) {
     $knick->set_voltage($volt);
     usleep(500000);
-    my $meas=$hp->read_voltage_dc(10,0.0001);
+    my $meas = $hp->read_voltage_dc(10,0.0001);
     $measurement->log_line($volt,$meas);
 }
 
-my $meta=$measurement->finish_measurement();
+my $meta = $measurement->finish_measurement();
 
