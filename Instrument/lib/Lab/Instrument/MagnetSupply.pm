c4b28368de62ca0745fe9b47c7056320e4a75cce
Andreas K. Hüttel <mail@akhuettel.de>
2009-04-17 17:31:22 +0000

    started with additional drivers
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@567 d7904494-db04-0410-88ca-e47d1dcc2d9a

---
+++ Instrument/lib/Lab/Instrument/MagnetSupply.pm
@@ -0,0 +1,316 @@
+#$Id$
+package Lab::Instrument::Source;
+use strict;
+use Time::HiRes qw(usleep gettimeofday);
+
+our $VERSION = sprintf("1.%04d", q$Revision$ =~ / (\d+) /);
+
+sub new {
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+
+    my $self = bless {}, $class;
+
+    %{$self->{default_config}}=%{shift @_};
+    %{$self->{config}}=%{$self->{default_config}};
+    $self->configure(@_);
+
+    $self->{_gp}->{last_voltage}=undef;
+    $self->{_gp}->{last_settime_mus}=undef;
+    
+    return $self;
+}
+
+sub configure {
+    my $self=shift;
+    #supported config options are (so far)
+    #   gate_protect
+    #   gp_max_volt_per_second
+    #   gp_max_volt_per_step
+    #   gp_max_step_per_second
+    #   gp_min_volt
+    #   gp_max_volt
+    #   qp_equal_level
+    my $config=shift;
+    if ((ref $config) =~ /HASH/) {
+        for my $conf_name (keys %{$self->{default_config}}) {
+            #print "Key: $conf_name, default: ",$self->{default_config}->{$conf_name},", old config: ",$self->{config}->{$conf_name},", new config: ",$config->{$conf_name},"\n";
+            unless ((defined($self->{config}->{$conf_name})) || (defined($config->{$conf_name}))) {
+                $self->{config}->{$conf_name}=$self->{default_config}->{$conf_name};
+            } elsif (defined($config->{$conf_name})) {
+                $self->{config}->{$conf_name}=$config->{$conf_name};
+            }
+        }
+        return $self;
+    } elsif($config) {
+        return $self->{config}->{$config};
+    } else {
+        return $self->{config};
+    }
+}
+
+sub set_voltage {
+    my $self=shift;
+    my $voltage=shift;
+    if ($self->{config}->{gate_protect}) {
+        $voltage=$self->sweep_to_voltage($voltage);
+    } else {
+        $self->_set_voltage($voltage);
+    }
+    $self->{_gp}->{last_voltage}=$voltage;
+    return $voltage;
+}
+
+sub step_to_voltage {
+    my $self=shift;
+    my $voltage=shift;
+    my $voltpersec=abs($self->{config}->{gp_max_volt_per_second});
+    my $voltperstep=abs($self->{config}->{gp_max_volt_per_step});
+    my $steppersec=abs($self->{config}->{gp_max_step_per_second});
+
+    #read output voltage from instrument (only at the beginning)
+    my $last_v=$self->{_gp}->{last_voltage};
+    unless (defined $last_v) {
+        $last_v=$self->get_voltage();
+        $self->{_gp}->{last_voltage}=$last_v;
+    }
+    
+    if (defined($self->{config}->{gp_max_volt}) && ($voltage > $self->{config}->{gp_max_volt})) {
+        $voltage = $self->{config}->{gp_max_volt};
+    }
+    if (defined($self->{config}->{gp_min_volt}) && ($voltage < $self->{config}->{gp_min_volt})) {
+        $voltage = $self->{config}->{gp_min_volt};
+    }
+
+    #already there
+    return $voltage if (abs($voltage - $last_v) < $self->{config}->{gp_equal_level});
+
+    #do the magic step calculation
+    my $wait = ($voltpersec < $voltperstep * $steppersec) ?
+        $voltperstep/$voltpersec : # ignore $steppersec
+        1/$steppersec;             # ignore $voltpersec
+    my $step=$voltperstep * ($voltage <=> $last_v);
+    
+    #wait if necessary
+    my ($ns,$nmu)=gettimeofday();
+    my $now=$ns*1e6+$nmu;
+    unless (defined (my $last_t=$self->{_gp}->{last_settime_mus})) {
+        $self->{_gp}->{last_settime_mus}=$now;
+    } elsif ( $now-$last_t < 1e6*$wait ) {
+        usleep ( ( 1e6*$wait+$last_t-$now ) );
+        ($ns,$nmu)=gettimeofday();
+        $now=$ns*1e6+$nmu;
+    } 
+    $self->{_gp}->{last_settime_mus}=$now;
+    
+    #do one step
+    if (abs($voltage-$last_v) > abs($step)) {
+        $voltage=$last_v+$step;
+    }
+    $voltage=0+sprintf("%.10f",$voltage);
+    
+    $self->_set_voltage($voltage);
+    $self->{_gp}->{last_voltage}=$voltage;
+    return $voltage;
+}
+
+sub sweep_to_voltage {
+    my $self=shift;
+    my $voltage=shift;
+
+    my $last;
+    my $cont=1;
+    while($cont) {
+        $cont=0;
+        my $this=$self->step_to_voltage($voltage);
+        unless ((defined $last) && (abs($last-$this) < $self->{config}->{gp_equal_level})) {
+            $last=$this;
+            $cont++;
+        }
+    }; #ugly
+    return $voltage;
+}
+
+sub _set_voltage {
+    warn '_set_voltage not implemented for this instrument';
+}
+
+sub get_voltage {
+    my $self=shift;
+    my $voltage=$self->_get_voltage(@_);
+    $self->{_gp}->{last_voltage}=$voltage;
+    return $voltage;
+}
+
+sub _get_voltage {
+    warn '_get_voltage not implemented for this instrument';
+}
+
+sub get_range() {
+    warn 'get_range not implemented for this instrument';
+}
+
+sub set_range() {
+    warn 'set_range not implemented for this instrument';
+}
+
+1;
+
+=head1 NAME
+
+Lab::Instrument::Source - Base class for voltage source instruments
+
+=head1 SYNOPSIS
+
+=head1 DESCRIPTION
+
+This class implements a general voltage source. It is meant to be
+inherited by instrument classes (virtual instruments), that implement
+real voltage sources (e.g. the
+L<Lab::Instrument::Yokogawa7651|Lab::Instrument::Yokogawa7651> class).
+
+The class provides a unified user interface for those virtual voltage sources
+to support the exchangeability of instruments.
+
+Additionally, this class provides a safety mechanism called C<gate_protect>
+to protect delicate samples. It includes automatic limitations of sweep rates,
+voltage step sizes, minimal and maximal voltages.
+
+As a user you are NOT supposed to create instances of this class, but rather
+instances of instrument classes that internally use this module!
+
+=head1 CONSTRUCTOR
+
+  $self=new Lab::Instrument::SafeSource(\%default_config,\%config);
+
+The constructor will only be used by instrument drivers that inherit this class,
+not by the user.
+
+The instrument driver (e.g. L<Lab::Instrument::KnickS252|Lab::Instrument::KnickS252>)
+has a constructor like this:
+
+  $knick=new Lab::Instrument::KnickS252({
+    GPIB_board      => $board,
+    GPIB_address    => $address,
+    
+    gate_protect    => $gp,
+    [...]
+  });
+
+=head1 METHODS
+
+=head2 configure
+
+  $self->configure(\%config);
+
+Supported configure options are all related to the safety mechanism:
+
+=over 2
+
+=item gate_protect
+
+Whether to use the automatic sweep speed limitation. Can be set to 0 (off) or 1 (on).
+If it is turned on, the output voltage will not be changed faster than allowed
+by the C<gp_max_volt_per_second>, C<gp_max_volt_per_step> and C<gp_max_step_per_second>
+values. These three parameters overdefine the allowed speed. Only two
+parameters are necessary. If all three are set, the smalles allowed sweep rate
+is chosen.
+
+Additionally the maximal and minimal output voltages are limited.
+
+This mechanism is useful to protect sensible samples, that are destroyed by
+abrupt voltage changes. One example is gate electrodes on semiconductor electronics
+samples, hence the name.
+
+=item gp_max_volt_per_second
+
+How much the output voltage is allowed to change per second.
+
+=item gp_max_volt_per_step
+
+How much the output voltage is allowed to change per step.
+
+=item gp_max_step_per_second
+
+How many steps are allowed per second.
+
+=item gp_min_volt
+
+The smallest allowed output voltage.
+
+=item gp_max_volt
+
+The largest allowed output voltage.
+
+=item qp_equal_level
+
+Voltages with a difference less than this value are considered equal.
+
+=back
+
+=head2 set_voltage
+
+  $new_volt=$self->set_voltage($voltage);
+
+Sets the output to C<$voltage> (in Volts). If the configure option C<gate_protect> is set
+to a true value, the safety mechanism takes into account the C<gp_max_volt_per_step>,
+C<gp_max_volt_per_second> etc. settings, by employing the C<sweep_to_voltage> method.
+
+Returns the actually set output voltage. This can be different from C<$voltage>, due
+to the C<gp_max_volt>, C<gp_min_volt> settings.
+
+=head2 step_to_voltage
+
+  $new_volt=$self->step_to_voltage($voltage);
+
+Makes one safe step in direction to C<$voltage>. The output voltage is not changed by more
+than C<gp_max_volt_per_step>. Before the voltage is changed, the methods waits if not
+enough times has passed since the last voltage change. For step voltage and waiting time
+calculation, the larger of C<gp_max_volt_per_second> or C<gp_max_step_per_second> is ignored
+(see code).
+
+Returns the actually set output voltage. This can be different from C<$voltage>, due
+to the C<gp_max_volt>, C<gp_min_volt> settings.
+
+=head2 sweep_to_voltage
+
+  $new_volt=$self->sweep_to_voltage($voltage);
+
+This method sweeps the output voltage to the desired value and only returns then.
+Uses the L</step_to_voltage> method internally, so all discussions of config options
+from there apply too.
+
+Returns the actually set output voltage. This can be different from C<$voltage>, due
+to the C<gp_max_volt>, C<gp_min_volt> settings.
+
+=head1 CAVEATS/BUGS
+
+Probably many.
+
+=head1 SEE ALSO
+
+=over 4
+
+=item L<Time::HiRes>
+
+Used internally for the sweep timing.
+
+=item L<Lab::Instrument::KnickS252>
+
+This class inherits the gate protection mechanism.
+
+=item L<Lab::Instrument::Yokogawa7651>
+
+This class inherits the gate protection mechanism.
+
+=back
+
+=head1 AUTHOR/COPYRIGHT
+
+This is $Id$
+
+Copyright 2004-2006 Daniel Schröer (L<http://www.danielschroeer.de>)
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
---
+++ Instrument/lib/Lab/Instrument/TMRC2.pm
@@ -0,0 +1,275 @@
+#$Id$
+
+package Lab::Instrument::TMRC2;
+use strict;
+use Lab::Instrument;
+use Lab::Instrument::TemperatureControl;
+
+our $VERSION = sprintf("0.%04d", q$Revision$ =~ / (\d+) /);
+
+our @ISA=('Lab::Instrument::TemperatureControl');
+
+my $default_config={
+    min_temp                => 0.025,
+    max_temp                => 1,
+    temp_tolerance          => 5,
+	control_file            => "buffin.txt",
+	result_file             => "buffout.txt",
+	control_path            => "C:\\Program Files\\Trmc2\\",
+};
+
+sub new {
+    my $proto = shift;
+    my @args=@_;
+    my $class = ref($proto) || $proto;
+    my $self = $class->SUPER::new($default_config,@args);
+    bless ($self, $class);
+
+    $self->{vi}=new Lab::Instrument(@args);
+    
+    return $self
+}
+
+sub _write_command {
+
+	my $command=shift;
+	my $filename=$self->{config}->{control_path}.$self->{config}->{control_file};
+	
+	open BUFFIN, ">$filename";
+	print BUFFIN "$command\n";
+	close BUFFIN;
+
+};
+
+sub _read_command_reply {
+
+	my $filename=$self->{config}->{control_path}.$self->{config}->{result_file};
+
+	open BUFFOUT, "<$filename";
+	my @text=<BUFFOUT>;
+	close BUFFOUT;
+	
+	return @text;
+};
+
+sub _set_temp {
+    my $self=shift;
+    my $temp=shift;
+
+	# somethign missing here
+}
+
+
+sub _get_temp {
+    my $self=shift;
+	
+	# something missing here
+}
+
+
+
+sub initialize {
+    my $self=shift;
+    $self->{vi}->Write('RC');
+}
+
+
+1;
+
+=head1 NAME
+
+Lab::Instrument::Yokogawa7651 - Yokogawa 7651 DC source
+
+=head1 SYNOPSIS
+
+    use Lab::Instrument::Yokogawa7651;
+    
+    my $gate14=new Lab::Instrument::Yokogawa7651(0,11);
+    $gate14->set_range(5);
+    $gate14->set_voltage(0.745);
+    print $gate14->get_voltage();
+
+=head1 DESCRIPTION
+
+The Lab::Instrument::Yokogawa7651 class implements an interface to the
+7651 voltage and current source by Yokogawa. This class derives from
+L<Lab::Instrument::Source> and provides all functionality described there.
+
+=head1 CONSTRUCTORS
+
+=head2 new($gpib_board,$gpib_addr)
+
+=head1 METHODS
+
+=head2 set_voltage($voltage)
+
+=head2 get_voltage()
+
+=head2 set_range($range)
+
+    Fixed voltage mode
+        2   10mV
+        3   100mV
+        4   1V
+        5   10V
+        6   30V
+
+    Fixed current mode
+        4   1mA
+        5   10mA
+        6   100mA
+
+=head2 get_info()
+
+Returns the information provided by the instrument's 'OS' command.
+
+=head2 output_on()
+
+Sets the output switch to on.
+
+=head2 output_off()
+
+Sets the output switch to off. The instrument outputs no voltage
+or current then, no matter what voltage you set.
+
+=head2 get_output()
+
+Returns the status of the output switch (0 or 1).
+
+=head2 initialize()
+
+=head2 set_voltage_limit($limit)
+
+=head2 set_current_limit($limit)
+
+=head2 get_status()
+
+Returns a hash with the following keys:
+
+    CAL_switch
+    memory_card
+    calibration_mode
+    output
+    unstable
+    error
+    execution
+    setting
+    
+The value for each key is either 0 or 1, indicating the status of the instrument.
+
+=begin html
+
+=head1 INSTRUMENT SPECIFICATIONS
+
+=head2 DC voltage
+
+The stability (24h) is the value at 23 ± 1°C. The stability (90days),
+accuracy (90days) and accuracy (1year) are values at 23 ± 5°C.
+The temperature coefficient is the value at 5 to 18°C and 28 to 40°C.
+
+ Range  Maximum     Resolution  Stability 24h   Stability 90d   
+        Output                  ±(% of setting  ±(% of setting  
+                                +µV)            +µV)            
+ ------------------------------------------------------------- 
+ 10mV   ±12.0000mV  100nV       0.002 + 3       0.014 + 4       
+ 100mV  ±120.000mV  1µV         0.003 + 3       0.014 + 5       
+ 1V     ±1.20000V   10µV        0.001 + 10      0.008 + 50      
+ 10V    ±12.0000V   100µV       0.001 + 20      0.008 + 100     
+ 30V    ±32.000V    1mV         0.001 + 50      0.008 + 200     
+
+
+
+ Range  Accuracy 90d    Accuracy 1yr    Temperature
+        ±(% of setting  ±(% of setting  Coefficient
+        +µV)            +µV)            ±(% of setting
+                                        +µV)/°C
+ -----------------------------------------------------
+ 10mV   0.018 + 4       0.025 + 5       0.0018 + 0.7
+ 100mV  0.018 + 10      0.025 + 10      0.0018 + 0.7
+ 1V     0.01 + 100      0.016 + 120     0.0009 + 7
+ 10V    0.01 + 200      0.016 + 240     0.0008 + 10
+ 30V    0.01 + 500      0.016 + 600     0.0008 + 30
+ 
+
+ 
+ Range   Maximum Output                   Output Noise
+         Output  Resistance          DC to 10Hz  DC to 10kHz
+                                                 (typical data)
+ ----------------------------------------------------------
+ 10mV    -       approx. 2Ohm        3µVp-p      30µVp-p
+ 100mV   -       approx. 2Ohm        5µVp-p      30µVp-p
+ 1V      ±120mA  less than 2mOhm     15µVp-p     60µVp-p
+ 10V     ±120mA  less than 2mOhm     50µVp-p     100µVp-p
+ 30V     ±120mA  less than 2mOhm     150µVp-p    200µVp-p
+
+
+Common mode rejection:
+120dB or more (DC, 50/60Hz). (However, it is 100dB or more in the
+30V range.)
+
+=head2 DC current
+
+ Range   Maximum     Resolution  Stability (24 h)    Stability (90 days) 
+         Output                  ±(% of setting      ±(% of setting      
+                                 + µA)               + µA)               
+ -----------------------------------------------------------------------
+ 1mA     ±1.20000mA  10nA        0.0015 + 0.03       0.016 + 0.1         
+ 10mA    ±12.0000mA  100nA       0.0015 + 0.3        0.016 + 0.5         
+ 100mA   ±120.000mA  1µA         0.004  + 3          0.016 + 5           
+
+
+
+ Range   Accuracy (90 days)  Accuracy (1 year)   Temperature  
+         ±(% of setting      ±(% of setting      Coefficient     
+         + µA)               + µA)               ±(% of setting  
+                                                 + µA)/°C        
+ -----   ------------------------------------------------------  
+ 1mA     0.02 + 0.1          0.03 + 0.1          0.0015 + 0.01   
+ 10mA    0.02 + 0.5          0.03 + 0.5          0.0015 + 0.1    
+ 100mA   0.02 + 5            0.03 + 5            0.002  + 1
+
+
+
+ Range  Maximum     Output                   Output Noise
+        Output      Resistance          DC to 10Hz  DC to 10kHz
+                                                    (typical data)
+ -----------------------------------------------------------------
+ 1mA    ±30 V       more than 100MOhm   0.02µAp-p   0.1µAp-p
+ 10mA   ±30 V       more than 100MOhm   0.2µAp-p    0.3µAp-p
+ 100mA  ±30 V       more than 10MOhm    2µAp-p      3µAp-p
+
+Common mode rejection: 100nA/V or more (DC, 50/60Hz).
+
+=end html
+
+=head1 CAVEATS
+
+probably many
+
+=head1 SEE ALSO
+
+=over 4
+
+=item Lab::VISA
+
+The Yokogawa7651 class uses the Lab::VISA module (L<Lab::VISA>).
+
+=item Lab::Instrument
+
+The Yokogawa7651 class is a Lab::Instrument (L<Lab::Instrument>).
+
+=item SafeSource
+
+The Yokogawa7651 class is a SafeSource (L<SafeSource>)
+
+=back
+
+=head1 AUTHOR/COPYRIGHT
+
+This is $Id$
+
+Copyright 2004 Daniel Schröer (L<http://www.danielschroeer.de>)
+
+This library is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
+
+=cut
