1d6860c139a237bddda0674a230d367e5d692992
Daniel Schröer <daniel@daimla1.de>
2010-04-17 21:44:23 +0000

    I am sorry. I had obviously forgotten to check in makedoku.pl.
    Now some cleanups with regard to dokutitle.
    
    PDF version does not go via ps anymore.
    
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@637 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- makedoku.pl
+++ makedoku.pl
@@ -13,30 +13,6 @@ my $dokudef = LoadFile('dokutoc.yaml');
 my $docdir = "Homepage/docs";
 my $tempdir = "Homepage/temp";
 
-my $processor = ($ARGV[0] =~ /html/) ? new Documentation::HTML($docdir) : new Documentation::LaTeX($docdir, $tempdir);   
+my $processor = ($ARGV[0] =~ /html/) ? new Documentation::HTML($docdir, $tempdir) : new Documentation::LaTeX($docdir, $tempdir);   
 
-$processor->start_index($dokudef->{title}, $dokudef->{authors});
-
-for my $section (@{$dokudef->{toc}}) {
-    walk_one_section($section, $dokudef->{title});
-}
-
-$processor->finish_index();
-
-sub walk_one_section {
-    my ($section, @sections) = @_;
-    my $title = (keys %$section)[0];
-    $processor->start_section($#sections + 2, $title);
-    push(@sections, $title);
-    for my $element (@{$section->{$title}}) {
-        unless (ref($element)) {
-            $processor->process_element($element, @sections);
-        }
-        elsif (ref($element) eq 'HASH') {
-            walk_one_section($element, @sections);
-        }
-        else {
-            die "You have messed up the toc file at ".(join "/", @sections)."/$element";
-        }
-    }
-}
+$processor->process($dokudef);
