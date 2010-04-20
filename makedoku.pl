92996dff513bee36c404ed3cf2dd7b8560ab7613
Daniel Schröer <daniel@daimla1.de>
2010-04-20 21:49:50 +0000

    Fix UTF-8 related problem. Reason was an incompatible change between YAML versions 0.70 and 0.71 :(
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@648 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- makedoku.pl
+++ makedoku.pl
@@ -1,13 +1,16 @@
 #!/usr/bin/perl
 
 use strict;
-use YAML qw(LoadFile);
+use YAML;
 use Documentation::LaTeX;
 use Documentation::HTML;
 use Data::Dumper;
 $Data::Dumper::Indent = 1;
 
-my $dokudef = LoadFile('dokutoc.yaml');
+open YAML, "<", 'dokutoc.yml' || die "Can't open toc: $!\n";
+my $yml = join "", <YAML>;
+close YAML;
+my $dokudef = Load($yml);
 #print Dumper($dokudef);
 
 my $docdir = "Homepage/docs";
