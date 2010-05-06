20b00c12f8580b86bb6ab7b6ddafd10636655362
Andreas K. Hüttel <mail@akhuettel.de>
2010-05-06 17:11:03 +0000

    Renamed docs/toc.php to docs/index.php
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@675 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- Documentation/Web.pm
+++ Documentation/Web.pm
@@ -20,7 +20,7 @@ sub new {
 
 sub start {
     my ($self, $title, $authors) = @_;
-    open $self->{index_fh}, ">", "$$self{docdir}/toc.php" or die $!;
+    open $self->{index_fh}, ">", "$$self{docdir}/index.php" or die $!;
     
     print {$self->{index_fh}} $self->_get_header($title);
  	print {$self->{index_fh}} qq{
@@ -51,7 +51,7 @@ sub process_element {
     $parser->parse_file($podfile);
     open OUTFILE, ">", "$$self{docdir}/$basename.php" or die;
         print OUTFILE $self->_get_header($title);
-        print OUTFILE qq(<h1><a href="toc.php">$sections[0]</a>: <span class="basename">$basename</span></h1>\n);
+        print OUTFILE qq(<h1><a href="index.php">$sections[0]</a>: <span class="basename">$basename</span></h1>\n);
         print OUTFILE $hascode ? qq{<p>(<a href="$basename\_source.php">Source code</a>)</p>} : "";
         print OUTFILE $html;
         print OUTFILE $self->_get_footer();
@@ -67,7 +67,7 @@ sub process_element {
         my $title = "$sections[0]: $basename";
         open SRCFILE, ">", "$$self{docdir}/$basename\_source.php" or die;
             print SRCFILE $self->_get_header($title);
-            print SRCFILE qq(<h1><a href="toc.php">$sections[0]</a>: <span class="basename">$basename</span></h1>\n);
+            print SRCFILE qq(<h1><a href="index.php">$sections[0]</a>: <span class="basename">$basename</span></h1>\n);
             print SRCFILE qq{<p>(<a href="$basename.php">Documentation</a>)</p>};
             print SRCFILE "<pre>$source</pre>\n";
             print SRCFILE $self->_get_footer();
