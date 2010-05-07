1ffa7ed8d03e6dae56bb47d5ac1aee651a3fd608
Daniel Schröer <daniel@daimla1.de>
2010-05-07 20:39:43 +0000

    Nachzügler
    git-svn-id: http://daimlerstr.de/svn/lab/trunk@678 d7904494-db04-0410-88ca-e47d1dcc2d9a

--- makedoku.pl
+++ makedoku.pl
@@ -6,6 +6,8 @@ use Documentation::LaTeX;
 use Documentation::HTML;
 use Documentation::Web;
 use Data::Dumper;
+use File::Copy;
+
 $Data::Dumper::Indent = 1;
 
 open YAML, "<", 'dokutoc.yml' || die "Can't open toc: $!\n";
@@ -14,16 +16,21 @@ close YAML;
 my $dokudef = Load($yml);
 #print Dumper($dokudef);
 
-my $docdir = "Homepage/docs";
+my $docdir  = "Homepage/docs";
 my $tempdir = "Homepage/temp";
 my $processor;
 
 # the autoupdater calls with parameter "web"
 
-for ($ARGV[0]) {
-	if	(/html/)	{ $processor = new Documentation::HTML($docdir, $tempdir); }
-	elsif	(/web/)		{ $processor = new Documentation::Web($docdir, $tempdir); }
-	else			{ $processor = new Documentation::LaTeX($docdir, $tempdir); }
+for ( $ARGV[0] ) {
+    if    (/pdf/) { $processor = new Documentation::LaTeX( $docdir, $tempdir ) }
+    elsif (/web/) {
+        $processor = new Documentation::Web(   $docdir, $tempdir );
+        copy('Homepage/index.html', 'Homepage/index.php');
+    }
+    else          { $processor = new Documentation::HTML(  $docdir, $tempdir ) }
 }
+
+
 
 $processor->process($dokudef);
