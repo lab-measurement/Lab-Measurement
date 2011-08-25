a9d2c22ce3433c1ae73d5343cc666a5fd547e7e9
Andreas K. Huettel <andreas.huettel@physik.uni-r.de>
2011-08-25 14:17:25 +0200

    add untested dc-diamond

--- Measurement/examples/QuantumDot/dc-diamond.pl
+++ Measurement/examples/QuantumDot/dc-diamond.pl
@@ -17,8 +17,17 @@ my $Vgatestart= 0;			# all gate voltages in V
 my $Vgatestop = 0.1;
 my $Vgatestep = 0.001;
 
-my $gateprotect = 1;			# 0=off 1=on
 my $Vgatemax = 7;			# for gateprotect
+
+#---bias settings---
+
+# we assume a 1:1000 voltage divider at the bias input
+
+my $Vbiasstart= -5;			# all bias voltages in mV
+my $Vbiasstop = 5;
+my $Vbiasstep = 0.01;
+
+my $Vbiasmax = 20;			# for "gateprotect"
 
 #---about the sample---
 
@@ -26,10 +35,10 @@ my $sample="DB_3224";
 
 #---about the setup---
 
-my $Vbias=-15;				# bias voltage in mV
 my $preampVpI=1.0e10;			# preamplification ( V/A )
 
 my $gpib_yoko_backgate = 3;
+my $gpib_yoko_bias = 4;
 my $gpib_hp = 12;
 
 ########## end configuration block ############
@@ -42,13 +51,13 @@ my $startstring=sprintf("%04u-%02u-%02u_%02u-%02u-%02u",
                 $starttime[2], $starttime[1], $starttime[0]);
 
 # filename
-my $filename=$startstring."_sample_".$sample."_gatesweep";
+my $filename=$startstring."_sample_".$sample."_diamond";
 
 # gate voltage source
 my $YokGate=new Lab::Instrument::Yokogawa7651({
     'connection_type' => 'LinuxGPIB',
     'gpib_address'  => $gpib_yoko_backgate,
-    'gate_protect'  => $gateprotect,
+    'gate_protect'  => 1,
     'gp_max_volt_per_second' => 0.05,
     'gp_max_step_per_second' => 10,
     'gp_max_volt_per_step' => 0.005,
@@ -57,6 +66,20 @@ my $YokGate=new Lab::Instrument::Yokogawa7651({
 });
 
 $YokGate->set_voltage($Vgatestart);
+
+# bias voltage source
+my $YokBias=new Lab::Instrument::Yokogawa7651({
+    'connection_type' => 'LinuxGPIB',
+    'gpib_address'  => $gpib_yoko_bias,
+    'gate_protect'  => 1,
+    'gp_max_volt_per_second' => 10,
+    'gp_max_step_per_second' => 10,
+    'gp_max_volt_per_step' => 1,
+    'gp_min_volt' => -$Vbiasmax,
+    'gp_max_volt'  => $Vbiasmax,
+});
+
+$YokBias->set_voltage($Vbiasstart);
 
 # current measurement multimeter (measures voltage at output of preamp)
 my $HP=new Lab::Instrument::HP34401A(
@@ -67,8 +90,9 @@ my $HP=new Lab::Instrument::HP34401A(
 # the comment text for the metadata
 my $comment=<<COMMENT;
 Sample $sample
-Gate sweep from $Vgatestart to $Vgatestop step $Vgatestep
-Bias $Vbias mV
+Coulomb diamond measurement
+Gate from $Vgatestart to $Vgatestop step $Vgatestep V
+Bias from $Vbiasstart to $Vbiasstop step $Vbiasstep mV
 Preamplifier setting $preampVpI V/A
 $startstring
 COMMENT
@@ -79,8 +103,8 @@ my $measurement=new Lab::Measurement(
     filename_base   => $filename,
     description     => $comment,
 
-    live_plot       => 'gatetrace',
-    live_refresh    => '1',
+    live_plot       => 'diamonds',
+    live_refresh    => '200',
 
     constants       => [
         {
@@ -93,6 +117,11 @@ my $measurement=new Lab::Measurement(
             'unit'          => 'V',
             'label'         => 'Gate voltage',
             'description'   => 'Applied to gate',
+        },
+        {
+            'unit'          => 'mV',
+            'label'         => 'Bias voltage',
+            'description'   => 'Applied to source',
         },
     	{
             'unit'          => 'A',
@@ -107,18 +136,25 @@ my $measurement=new Lab::Measurement(
             'label'         => 'gate voltage',
             'description'   => 'Applied to backgate via 5Hz filter.',
         },
+        {
+            'unit'          => 'mV',
+            'expression'    => '$C1',
+            'label'         => 'bias voltage',
+            'description'   => 'Applied to source.',
+        },
 	{
             'unit'          => 'A',
-            'expression'    => '$C1',
+            'expression'    => '$C2',
             'label'         => 'meas dc current through sample.',
             'description'   => 'Measured current through $sample',
         }
     ],
     plots           => {
-        'gatetrace'    => {
-            'type'          => 'line',
+        'diamonds'    => {
+            'type'          => 'pm3d',
             'xaxis'         => 0,
             'yaxis'         => 1,
+	    'cbaxis'        => 2,
             'grid'          => 'xtics ytics',
         },
     },
@@ -130,6 +166,7 @@ my $measurement=new Lab::Measurement(
 
 # all setup is done, now comes the actual measurement
 
+# for the gate:
 
 # we have to make sure that the step size has the correct sign to 
 # go from start to stop 
@@ -138,10 +175,19 @@ if (($Vgatestop-$Vgatestart)/$Vgatestep<0) { $Vgatestep=-$Vgatestep; };
 # which way are we going?
 my $stepsign_gate=$Vgatestep/abs($Vgatestep);
 
-# every "trace" in a measurement has to start with this
-$measurement->start_block();
+# for the bias:
 
-# the measurement loop
+# we have to make sure that the step size has the correct sign to 
+# go from start to stop 
+if (($Vbiasstop-$Vbiasstart)/$Vbiasstep<0) { $Vbiasstep=-$Vbiasstep; };
+
+# which way are we going?
+my $stepsign_bias=$Vbiasstep/abs($Vbiasstep);
+
+
+
+
+# the outer measurement loop
 for (my $Vgate=$Vgatestart;
      $stepsign_gate*$Vgate<=$stepsign_gate*$Vgatestop; 
      $Vgate+=$Vgatestep) {
@@ -149,15 +195,36 @@ for (my $Vgate=$Vgatestart;
 	# set gate voltage
 	$YokGate->set_voltage($Vgate);
 
-	# read out multimeter
-	my $measVpreamp =$HP->query("READ?");
+	# set bias voltage
+	$YokBias->set_voltage($Vbiasstart);
 
-	# make sure the result does not contain any linefeeds anymore
-	chomp $measVpreamp;
+	# wait a moment
+	sleep(1);
 
-	# write into datafile
-	$measurement->log_line($Vgate, $measVpreamp/$preampVpI);
+	# every "trace" in a measurement has to start with this
+	$measurement->start_block();
+
+	# the inner measurement loop
+	for (my $Vbias=$Vbiasstart;
+	    $stepsign_bias*$Vbias<=$stepsign_bias*$Vbiasstop; 
+	    $Vbias+=$Vbiasstep) {
+
+		# set bias voltage
+		$YokBias->set_voltage($Vbias);
+
+		# read out multimeter
+		my $measVpreamp =$HP->query("READ?");
+
+		# make sure the result does not contain any linefeeds
+		chomp $measVpreamp;
+
+		# write into datafile
+		$measurement->log_line($Vgate, $Vbias,
+				       $measVpreamp/$preampVpI);
+
+	}
 }
+
 
 # and we're done
 my $meta=$measurement->finish_measurement();
