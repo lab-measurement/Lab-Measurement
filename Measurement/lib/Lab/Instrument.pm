f9ce3d0157bd473057d9e3f42176888b62ba809c
Andreas K. Huettel <andreas.huettel@physik.uni-r.de>
2011-09-19 18:16:26 +0200

    Compare strings not numbers

--- Measurement/lib/Lab/Instrument.pm
+++ Measurement/lib/Lab/Instrument.pm
@@ -202,7 +202,7 @@ sub _setconnection { # $self->setconnection() create new or use existing connect
 
 		if( $connection_type !~ /^[A-Za-z0-9_\-\:]*$/ ) { Lab::Exception::CorruptParameter->throw( error => "Given connection type is does not look like a valid module name.\n" . Lab::Exception::Base::Appendix()); };
 
-		if( $connection_type == 'none' ) { return; };
+		if( $connection_type eq 'none' ) { return; };
 		# todo: allow this only iff the device supports connection_type none
 
 		$full_connection = "Lab::Connection::" . $connection_type;
