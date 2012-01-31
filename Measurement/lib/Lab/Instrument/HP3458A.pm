c3040c9939a8bfccbf17ad2a7f34b54ee672ee50
Florian Olbrich <florian.olbrich@physik.uni-regensburg.de>
2012-01-31 17:09:43 +0100

    refined get_status() for HP3458A.

--- Measurement/lib/Lab/Instrument/HP3458A.pm
+++ Measurement/lib/Lab/Instrument/HP3458A.pm
@@ -381,9 +381,11 @@ sub beep {
 
 sub get_status {
 	my $self=shift;
-	my %status = ();
-	($status{PRG_COMPLETE}, $status{LIMIT_EXCEEDED}, $status{SRQ_EXECUTED}, $status{POWER_ON}, $status{READY}, $status{ERROR}, $status{SRQ}, $status{DATA} ) = $self->connection()->serial_poll();
-	return %status;	
+	my $request = shift;
+	my $status = {};
+	($status->{PRG_COMPLETE}, $status->{LIMIT_EXCEEDED}, $status->{SRQ_EXECUTED}, $status->{POWER_ON}, $status->{READY}, $status->{ERROR}, $status->{SRQ}, $status->{DATA} ) = $self->connection()->serial_poll();
+	return $status->{$request} if defined $request;
+	return $status;
 }
 
 sub get_error {
