c12b04e0cba07cd8945124b4d1f22130d16b0533
Hermann Kraus <hermr2d2@gmail.com>
2012-06-18 15:42:36 +0200

    Connect by USB id.

--- Measurement/examples/FrequencySweep/sweep.pl
+++ Measurement/examples/FrequencySweep/sweep.pl
@@ -5,13 +5,12 @@ use Lab::Instrument::U2000;
 use Time::HiRes qw(usleep);
 
 my $signal=new Lab::Instrument::HP83732A(
-    connection_type=>'LinuxGPIB',
-    gpib_address => 19,
+    connection_type=>'USBtmc',
+    visa_name => 'USB::0x0AAD::0x0054::12345::INSTR'
 );
 
 my $powermeter=new Lab::Instrument::U2000(
     connection_type=>'USBtmc',
-    tmc_address => 0,
 );
 
 sub set_freq
@@ -19,7 +18,7 @@ sub set_freq
     my $freq = shift;
     $signal->set_cw($freq);
     $powermeter->set_frequency($freq);
-    usleep(200000);
+    $signal->query("*OPC?");
 }
 
 my $error = $powermeter->get_error();
