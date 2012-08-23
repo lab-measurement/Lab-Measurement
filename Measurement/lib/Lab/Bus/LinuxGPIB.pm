168bf4f629a4b120f0ed10dc2dbdecfe9511a551
Hermann Kraus <hermr2d2@gmail.com>
2012-08-23 14:25:48 +0200

    Automatically go to local mode on program end.

--- Measurement/lib/Lab/Bus/LinuxGPIB.pm
+++ Measurement/lib/Lab/Bus/LinuxGPIB.pm
@@ -303,8 +303,8 @@ sub serial_poll {
 sub connection_clear {
 	my $self = shift;
 	my $connection_handle=shift;
-
 	ibclr($connection_handle->{'gpib_handle'});
+	ibloc($connection_handle->{'gpib_handle'});
 }
 
 sub timeout {
