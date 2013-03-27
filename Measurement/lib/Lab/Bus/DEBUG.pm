2d663b74029c0e9b5deb5887ec7d8088f411c8b5
Andreas K. Huettel <mail@akhuettel.de>
2013-03-27 13:56:27 +0100

    ... or not. :(

--- Measurement/lib/Lab/Bus/DEBUG.pm
+++ Measurement/lib/Lab/Bus/DEBUG.pm
@@ -12,7 +12,7 @@ use Time::HiRes qw (usleep sleep);
 use Lab::Bus;
 use Data::Dumper;
 use Carp;
-use Lab::BUS::DEBUG::HumanInstrument;
+use Lab::Bus::DEBUG::HumanInstrument;
 
 use Lab::Exception
 
