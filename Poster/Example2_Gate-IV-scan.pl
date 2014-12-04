623e834de16e597252d981325fa24d28397acbe3
Andreas K. Huettel <dilfridge@gentoo.org>
2014-12-04 17:35:48 +0100

    Clean up example script, insert XPRESS into structure drawing

--- Poster/Example2_Gate-IV-scan.pl
+++ Poster/Example2_Gate-IV-scan.pl
@@ -15,7 +15,8 @@ my $multimeter = Instrument('Agilent34410A',
 	{
 	connection_type => 'VISA_GPIB',
 	gpib_address => 17,
-	nplc => 10					# integration time in number of powerline cylces [10*(1/50)]
+	nplc => 10			# integration time in number of 
+					# powerline cylces [10*(1/50)]
 	});
 
 my $gate = Instrument('Yokogawa7651', 
@@ -34,19 +35,21 @@ my $gate_sweep = Sweep('Voltage',
 	{
 	mode => 'step',
 	instrument => $gate,
-	points => [-5, 5],	# [starting point, target] in Volts
+	points => [-5, 5],		# [starting point, target] in Volt
 	stepwidth => [0.1],
-	rate => [5e-3],			# [rate to approach start, sweeping rate for measurement] in Volts/s
+	rate => [5e-3],			# [rate to approach start, sweeping 
+					# rate for measurement] in Volt/s
 	});
 
 my $bias_sweep = Sweep('Voltage', 
 	{
 	instrument => $bias,
-	points => [-5e-3, 5e-3],	# [starting point, target] in Volts
-	rate => [0.1, 0.5e-3],		# [rate to approach start, sweeping rate for measurement] in Volts/s
-	interval => 1, 				# measurement interval in s
+	points => [-5e-3, 5e-3],	# [starting point, target] in Volt
+	rate => [0.1, 0.5e-3],		# [rate to approach start, sweeping 
+					# rate for measurement] in Volt/s
+	interval => 1, 			# measurement interval in s
 
-	delay_before_loop => 10 	# delay before Sweep begins in s
+	delay_before_loop => 10 	# delay before sweep begins in s
 	});
 
 #-------- 3. Create a DataFile -------------
@@ -58,25 +61,22 @@ $DataFile->add_column('BiasVoltage');
 $DataFile->add_column('Current');
 $DataFile->add_column('Resistance');
 
-
 $DataFile->add_plot({
 	'type'	  => 'pm3d'
-
 	'x-axis'  => 'GateVoltage',
 	'y-axis'  => 'BiasVoltage',
 	'cb-axis' => 'Current'
 	});
 
-	
 #-------- 4. Measurement Instructions -------
 
 my $my_measurement = sub {
 
 	my $sweep = shift;
 
-	my $gate_voltage = $gate->get_value();
-	my $bias_voltage = $bias->get_value();
-	my $current = $multimeter->get_value()*1e-7;
+	my $gate_voltage = $gate->get_value();		# gate voltage source
+	my $bias_voltage = $bias->get_value();		# bias voltage source
+	my $current = $multimeter->get_value()*1e-7;	# I/V converter output
 	my $resistance = ($current != 0) ? $voltage/$current : '?';
 
 	$sweep->LOG({
@@ -94,8 +94,8 @@ $DataFile->add_measurement($my_measurement);
 $voltage_sweep->add_DataFile($DataFile);
 
 my $frame = Frame();
-$frame->add_master($gate_sweep);
-$frame->add_slave($bias_sweep);
+$frame->add_master($gate_sweep);			# the outer, slow loop
+$frame->add_slave($bias_sweep);				# the inner, fast loop
 
 $frame->start();
 
