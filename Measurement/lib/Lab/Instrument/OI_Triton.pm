c9a46aef5b4937f84aca60de5e04af8e3f2b885a
Andreas K. Huettel <andreas.huettel@ur.de>
2015-07-30 17:27:42 +0200

    Add untested temperature control functions

--- Measurement/lib/Lab/Instrument/OI_Triton.pm
+++ Measurement/lib/Lab/Instrument/OI_Triton.pm
@@ -37,6 +37,61 @@ sub get_temperature {
   return $temp;
 };
 
+sub enable_control{
+  my $self = shift;
+  my $temp = $self->query("SET:SYS:USER:NORM\n");
+  # typical response: STAT:SET:SYS:USER:NORM:VALID
+  return $temp;
+};
+
+sub disable_control{
+  my $self = shift;
+  my $temp = $self->query("SET:SYS:USER:GUEST\n");
+  # typical response: STAT:SET:SYS:USER:GUEST:VALID
+  return $temp;
+};
+
+sub enable_temp_pid{
+  my $self = shift;
+  my $temp = $self->query("SET:DEV:T5:TEMP:LOOP:MODE:ON\n");
+  # typical response: STAT:SET:DEV:T5:TEMP:LOOP:MODE:ON:VALID
+  return $temp;
+};
+
+sub disable_temp_pid{
+  my $self = shift;
+  my $temp = $self->query("SET:DEV:T5:TEMP:LOOP:MODE:OFF\n");
+  # typical response: STAT:SET:DEV:T5:TEMP:LOOP:MODE:OFF:VALID
+  return $temp;
+};
+
+sub get_T{
+  my $self = shift;
+  my $temp = $self->get_temperature("5");
+  return $temp;
+};
+
+
+sub waitfor_T{
+  my $self = shift;
+  my $target = shift;
+  my $now = 10000000;
+
+  do {
+    sleep(10);
+    $now = get_T();
+    print "Waiting for T=$target ; current temperature is T=$now\n";
+  } unless (abs($now-$target)/$target < 0.05);
+};
+
+sub set_T{
+  my $self = shift;
+  my $temperature = shift;
+  my $temp = $self->query("SET:DEV:T5:TEMP:LOOP:TSET:$temperature\n");
+  # typical reply: STAT:SET:DEV:T5:TEMP:LOOP:TSET:0.1:VALID
+  waitfor_T($temperature);
+  waitfor_T($temperature);
+};
 
 
 1;
