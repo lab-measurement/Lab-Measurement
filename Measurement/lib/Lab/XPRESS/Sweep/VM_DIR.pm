b4a278f1fcde64d368a8afa727e216079ad4643c
buc <christian@ChBs-MBP.fritz.box>
2016-03-03 12:02:51 +0100

    Moved N-Dimensional Sweeps to correct directory (XPRESS/Sweep not Instruments)

--- Measurement/lib/Lab/XPRESS/Sweep/VM_DIR.pm
+++ Measurement/lib/Lab/XPRESS/Sweep/VM_DIR.pm
@@ -2,19 +2,17 @@ package Lab::XPRESS::Sweep::VM_DIR;
 
 our $VERSION = '3.500';
 
-use Lab::XPRESS::Sweep::Sweep_ND;
+use Lab::XPRESS::Sweep::SweepND;
 use Time::HiRes qw/usleep/, qw/time/;
 use strict;
 
 our @ISA=('Lab::XPRESS::Sweep::SweepND');
 
 
-
-
 sub new {
     my $proto = shift;
 	my @args=@_;
-    my $class = ref($proto) || $proto; 
+    my $class = ref($proto) || $proto;
 	my $self->{default_config} = {
 		id => 'VectorMagnet_sweep',
 		filename_extension => 'VM_DIR=',
@@ -25,28 +23,28 @@ sub new {
 		duration	=> [],
 		mode	=> 'continuous',
 		coordinate_system => 'cartesian', # or 'spherical'
-		allowed_instruments => ['Lab::Instrument::Vectormagnet'],
+		allowed_instruments => ['Lab::Instrument::Vectormagnet', 'Lab::Instrument::VectormagnetNoY'],
 		allowed_sweep_modes => ['continuous', 'list', 'step'],
 		number_of_points => [undef]
 		};
-		
-	$self = $class->SUPER::new($self->{default_config},@args);	
+
+	$self = $class->SUPER::new($self->{default_config},@args);
 	bless ($self, $class);
     return $self;
 }
 
 sub go_to_sweep_start {
 	my $self = shift;
-	
+
 	# go to start:
 	print "going to start ... ";
-		
+
 	@{$self->{config}->{instrument}}[0]->config_DIR_sweep(@{@{$self->{config}->{points}}[0]},@{$self->{config}->{rate}}[0],1,$self->{config}->{coordinate_system});
 	@{$self->{config}->{instrument}}[0]->trg();
 	@{$self->{config}->{instrument}}[0]->wait();
-	
+
 	print "done\n";
-	
+
 }
 
 sub start_continuous_sweep {
@@ -55,15 +53,15 @@ sub start_continuous_sweep {
 	# continuous sweep:
 	@{$self->{config}->{instrument}}[0]->config_DIR_sweep(@{@{$self->{config}->{points}}[$self->{sequence}+1]},@{$self->{config}->{rate}}[$self->{sequence}+1],1,$self->{config}->{coordinate_system});
 	@{$self->{config}->{instrument}}[0]->trg();
-		
+
 }
 
 
 sub go_to_next_step {
 	my $self = shift;
 
-	
-	# step mode:	
+
+	# step mode:
 	@{$self->{config}->{instrument}}[0]->config_DIR_sweep(@{@{$self->{config}->{points}}[$self->{iterator}]},@{$self->{config}->{rate}}[$self->{iterator}],1,$self->{config}->{coordinate_system});
 	@{$self->{config}->{instrument}}[0]->trg();
 	@{$self->{config}->{instrument}}[0]->wait();
@@ -75,7 +73,7 @@ sub exit_loop {
 	if (not @{$self->{config}->{instrument}}[0]->active() )
 		{
 		if ( $self->{config}->{mode} =~ /step|list/ )
-			{	
+			{
 			if (not defined @{$self->{config}->{points}}[$self->{iterator}+1])
 				{
 				return 1;
@@ -86,7 +84,7 @@ sub exit_loop {
 				}
 			}
 		elsif ( $self->{config}->{mode} eq "continuous" )
-			{	
+			{
 			if (not defined @{$self->{config}->{points}}[$self->{sequence}+2])
 				{
 				return 1;
@@ -96,7 +94,7 @@ sub exit_loop {
 			@{$self->{config}->{instrument}}[0]->trg();
 
 			}
-			
+
 			return 0;
 		}
 	else
@@ -134,22 +132,22 @@ sub exit {
 
 	use Lab::XPRESS::hub;
 	my $hub = new Lab::XPRESS::hub();
-	
-	
-	my $IPS = $hub->Instrument('IPS', 
+
+
+	my $IPS = $hub->Instrument('IPS',
 		{
 		connection_type => 'VISA_GPIB',
 		gpib_address => 24
 		});
-	
+
 	my $sweep_magnet = $hub->Sweep('Magnet',
 		{
 		instrument => $IPS,
 		points => [-10,10],
 		rate => [1.98,1],
-		mode => 'continuous',		
+		mode => 'continuous',
 		interval => 1,
-		backsweep => 1 
+		backsweep => 1
 		});
 
 .
@@ -163,16 +161,16 @@ The Lab::XPRESS::Sweep::Magnet class implements a module for magnetic field Swee
 .
 
 =head1 CONSTRUCTOR
-	
+
 
 	my $sweep_magnet = $hub->Sweep('Magnet',
 		{
 		instrument => $IPS,
 		points => [-10,10],
 		rate => [1.98,1],
-		mode => 'continuous',		
+		mode => 'continuous',
 		interval => 1,
-		backsweep => 1 
+		backsweep => 1
 		});
 
 Instantiates a new Magnet-sweep.
@@ -183,28 +181,28 @@ Instantiates a new Magnet-sweep.
 
 =head2 instrument [Lab::Instrument] (mandatory)
 
-Instrument, conducting the sweep. Must be of type Lab:Instrument. 
+Instrument, conducting the sweep. Must be of type Lab:Instrument.
 Allowed instruments: Lab::Instrument::IPS, Lab::Instrument::IPSWeiss1, Lab::Instrument::IPSWeiss2, Lab::Instrument::IPSWeissDillFridge
 
 .
 
 =head2 mode [string] (default = 'continuous' | 'step' | 'list')
-	
+
 continuous: perform a continuous magnetic field sweep. Measurements will be performed constantly at the time-interval defined in interval.
 
 step: measurements will be performed at discrete values of the magnetic field between start and end points defined in parameter points, seperated by the magnetic field values defined in parameter stepwidth
 
 list: measurements will be performed at a list of magnetic field values defined in parameter points
-	
+
 .
 
 =head2 points [float array] (mandatory)
 
 array of magnetic field values (in Tesla) that defines the characteristic points of the sweep.
-First value is appraoched before measurement begins. 
+First value is appraoched before measurement begins.
 
 Case mode => 'continuous' :
-List of at least 2 values, that define start and end point of the sweep or a sequence of consecutive sweep-sections (e.g. if changing the sweep-rate for different sections or reversing the sweep direction). 
+List of at least 2 values, that define start and end point of the sweep or a sequence of consecutive sweep-sections (e.g. if changing the sweep-rate for different sections or reversing the sweep direction).
 	 	points => [-5, 5]	# Start: -5 / Stop: 5
 
 		points => [-5, -1, 1, 5]
@@ -223,17 +221,17 @@ Array of magnetic field values, with minimum length 1, that are approached in se
 
 array of rates, at which the magnetic field is swept (Tesla / min).
 Has to be of length 1 or greater (Maximum length: length of points-array).
-The first value defines the rate to approach the starting point. 
+The first value defines the rate to approach the starting point.
 The following values define the rates to approach the magnetic field values defined by the points-array.
 If the number of values in the rates-array is less than the length of the points-array, the last defined rate will be used for the remaining sweep sections.
 
-	
+
 	points => [-5, -1, 1, 5],
 	rates => [1, 0.5, 0.2]
-	
-	rate to approach -5 T (the starting point): 1 T/min 
-	rate to approach -1 T  : 0.5 T/min 
-	rate to approach 1 T  : 0.2 T/min 
+
+	rate to approach -5 T (the starting point): 1 T/min
+	rate to approach -1 T  : 0.5 T/min
+	rate to approach 1 T  : 0.2 T/min
 	rate to approach 5 T   : 0.2 T/min (last defined rate)
 
 .
@@ -242,8 +240,8 @@ If the number of values in the rates-array is less than the length of the points
 =head2 duration [float array] (mandatory if not defined rate)
 
 can be used instead of 'rate'. Attention: Use only the 'duration' or the 'rate' parameter. Using both will cause an Error!
-	
-The first value defines the duration to approach the starting point. 
+
+The first value defines the duration to approach the starting point.
 The second value defines the duration to approach the magnetic field value defined by the second value of the points-array.
 ...
 If the number of values in the duration-array is less than the length of the points-array, last defined duration will be used for the remaining sweep sections.
@@ -252,13 +250,13 @@ If the number of values in the duration-array is less than the length of the poi
 
 =head2 stepwidth [float array]
 
-This parameter is relevant only if mode = 'step' has been selected. 
-Stepwidth has to be an array of length '1' or greater. The values define the width for each step within the corresponding sweep sequence. 
+This parameter is relevant only if mode = 'step' has been selected.
+Stepwidth has to be an array of length '1' or greater. The values define the width for each step within the corresponding sweep sequence.
 If the length of the defined sweep sequence devided by the stepwidth is not an integer number, the last step will be smaller in order to reach the defined points-value.
-	
+
 	points = [0, 0.5, 3]
 	stepwidth = [0.2, 0.5]
-	
+
 	==> steps: 0, 0.2, 0.4, 0.5, 1.0, 1.5, 2.0, 2.5, 3
 
 .
@@ -266,12 +264,12 @@ If the length of the defined sweep sequence devided by the stepwidth is not an i
 =head2 number_of_points [int array]
 
 can be used instead of 'stepwidth'. Attention: Use only the 'number_of_points' or the 'stepwidth' parameter. Using both will cause an Error!
-This parameter is relevant only if mode = 'step' has been selected. 
+This parameter is relevant only if mode = 'step' has been selected.
 Number_of_points has to be an array of length '1' or greater. The values defines the number of steps within the corresponding sweep sequence.
-	
+
 	points = [0, 0.5, 3]
 	number_of_points = [5, 2]
-	
+
 	==> steps: 0, 0.1, 0.2, 0.3, 0.4, 0.5, 1.75, 3
 
 .
@@ -287,7 +285,7 @@ interval in seconds for taking measurement points. Only relevant in mode 'contin
 0 : no backsweep (default)
 1 : a backsweep will be performed
 2 : no backsweep performed automatically, but sweep sequence will be reverted every second time the sweep is started (relevant eg. if sweep operates as a slave. This way the sweep sequence is reverted at every second step of the master)
-	
+
 .
 
 =head2 id [string] (default = 'Magnet_sweep')
@@ -310,7 +308,7 @@ defines the time in seconds to wait after the starting point has been reached.
 
 =head2 delay_in_loop [int] (default = 0)
 
-This parameter is relevant only if mode = 'step' or 'list' has been selected. 
+This parameter is relevant only if mode = 'step' or 'list' has been selected.
 Defines the time in seconds to wait after the value for the next step has been reached.
 
 .
@@ -339,7 +337,7 @@ probably none
 
 =head1 AUTHOR/COPYRIGHT
 
-Christian Butschkow and Stefan Geißler
+Christian Butschkow and Stefan Geiï¿½ler
 
 This library is free software; you can redistribute it and/or modify it under the same
 terms as Perl itself.
