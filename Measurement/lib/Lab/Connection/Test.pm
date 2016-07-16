d2bee449ac1bc729c3856a7bf4a0dc58621d112f
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2016-07-16 14:50:51 +0200

    First draft of the Test Connection.
    Together with connection logging, this will enable us to perform
    automated testing (without physically connected instruments).

---
+++ Measurement/lib/Lab/Connection/Test.pm
@@ -0,0 +1,87 @@
+package Lab::Connection::Test;
+
+use warnings;
+use strict;
+use 5.010;
+
+use YAML::XS qw/Dump LoadFile/;
+use autodie;
+use Carp;
+
+use Data::Compare;
+use parent 'Lab::Connection';
+
+my %fields = (
+    log_file => undef,
+    log_index => 0,
+    );
+
+around 'new' => sub {
+    my $orig = shift;
+    my $proto = shift;
+    my $class = ref($proto) || $proto;
+    my $twin  = undef;
+
+    # getting fields and _permitted from parent class
+    my $self = $class->$orig(@_); 
+    
+    $self->_construct($class);
+
+    # open the log file
+    my $log_file = $self->log_file();
+    if (not defined $log_file) {
+	croak 'missing "log_file" parameter in connection';
+    }
+
+    my @logs = LoadFile($log_file);
+    $self->log_list(@logs);
+    
+    return $self;
+}
+
+sub process_call {
+    my $method = shift;
+    my $self = shift;
+    my @args = @_;
+    
+    my $index = $self->log_index();
+
+    my $received = [$index, $method, @args];
+    my $expected = $self->log_list()->[$index];
+
+    my $retval = splice @{$expected}, 1, 1;
+
+    if (not Compare($received, $expected)) {
+	die "Test connection:\nreceived:\n------------------\n", Dump($received), "----------------\nexpected:\n", Dump(\@expected);
+    }
+    
+    $self->log_index(++$index);
+    return $retval;
+}
+
+sub Clear {
+    return process_call('Clear', @_);
+}
+
+sub Write {
+    return process_call('Write', @_);
+}
+
+sub Read {
+    return process_call('Read', @_);
+}
+
+sub timeout {
+    return process_call('timeout', @_);
+}
+
+sub block_connection {
+    return process_call('block_connection', @_);
+}
+
+sub unblock_connection {
+    return process_call('unblock_connection', @_);
+}
+
+
+1;
