a3a25a900b72ab87e5d7339393612a2ecd619980
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2016-08-06 18:35:15 +0200

    LogBlock: simplify, add docs, add example.

--- Measurement/lib/Lab/XPRESS/Sweep/LogBlock.pm
+++ Measurement/lib/Lab/XPRESS/Sweep/LogBlock.pm
@@ -9,8 +9,43 @@ use Carp;
 
 use Data::Dumper;
 
+=pod
+
+=head1 NAME
+
+Lab::XPRESS::Sweep::LogBlock -- Sweep add-on for matrix logging.
+
+=head1 SYNOPSIS
+
+   $sweep->LogBlock(
+	prefix => [$gate, $bias],
+	block => $matrix);
+
+=head1 DESCRIPTION
+    
+This role exports the single method C<LogBlock>. The valid parameters are:
+
+=over
+
+=item block (mandatory)
+
+List of rows (e.g. C<[[1, 2, 3], [2, 3, 4]]>), which shell be written
+ to the data file.
+
+=item prefix
+
+List of parameters which shell be prefixed to each row of the block.
+
+=item file
+
+Number of the target data file (default: 0).
+
+=back
+    
+=cut
+
 sub LogBlock {
-    my $self = shift;
+    my $sweep = shift;
 
     if (@_ % 2 != 0) {
 	croak "expected hash";
@@ -23,43 +58,48 @@ sub LogBlock {
 	croak "missing mandatory parameter 'block'";
     }
 
-    my $header = $args{header};
-    if (not defined $header) {
-	croak "missing mandatory parameter 'header'";
+    my $prefix = $args{prefix};
+    if (not defined $prefix) {
+	$prefix = [];
     }
 
+    my $prefix_len = @$prefix;
+    
     my $file = $args{datafile};
     if (not defined $file) {
 	$file = 0;
     }
 
-    my $external_params = $args{external_params};
-    if (not defined $external_params) {
-	$external_params = {};
-    }
+    
+    
+    my $num_rows = @$block;
+    my $row_len = @{$block->[0]};
 
+    # Extract column header from the DataFile
+    my $datafile = $sweep->{DataFiles}[$file];
+    my $columns = $datafile->add_column();
+    my $columns_len = @$columns;
     
-    my $rows = @$block;
-    my $columns = @{$block->[0]};
-    my $header_columns = @$header;
-    
-    if ($columns != $header_columns) {
-	croak "header has $header_columns entries, block has $columns columns";
+    if ($row_len + $prefix_len != $columns_len) {
+	croak "The datafile expects $columns_len columns.\n"
+	    . "You only supplied $prefix_len + $row_len columns.";
     }
     
     # Write external parameters and block to datafile
-    
-    for (my $row = 0; $row < $rows; ++$row) {
-	my %log = %$external_params;
-	
-	for (my $col = 0; $col < $columns; ++$col) {
-	    $log{$header->[$col]} = $block->[$row][$col];
+
+    while (my ($i, $row) = each (@$block)) {
+	my %log;
+	unshift @$row, @$prefix;
+
+	while (my ($j, $key) = each (@$columns)) {
+	    $log{$key} = $row->[$j];
 	}
-	$self->LOG({%log}, $file);
 	
-	if ($row != $rows - 1) {
+	$sweep->LOG({%log}, $file);
+	
+	if ($i != $num_rows - 1) {
 	    # the last writeLOG is called in Sweep.pm
-	    $self->write_LOG();
+	    $sweep->write_LOG();
 	}
 	
     }
---
+++ Measurement/xt/logblock.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/env perl
+use 5.010;
+use warnings;
+use strict;
+use lib '../lib';
+use Lab::Measurement;
+use Data::Dumper;
+
+my $instr = Instrument('DummySource', {connection_type => 'DEBUG',
+				       gate_protect => 0});
+
+
+my $sweep = Sweep('Voltage', {
+    instrument => $instr,
+    mode => 'step',
+    jump => 1,
+    points => [0,1],
+    stepwidth => [0.1],
+    rate => [0.1],
+		  });
+
+
+my $DataFile = DataFile('somefile', 'blocklog');
+
+$DataFile->add_column('volt');
+$DataFile->add_column('frequency');
+$DataFile->add_column('s11');
+
+say Dumper $DataFile->add_column();
+
+my $measurement = sub {
+    my $sweep = shift;
+    
+    my $voltage = $sweep->get_value();
+
+    my $matrix = # $vna->get_trace(...)
+	[[1 + $voltage, 2 + $voltage],
+	 [2, 4],
+	 [3, 6],
+	 [4, 8],];
+
+    $sweep->LogBlock(
+	prefix => [$voltage],
+	block => $matrix);
+};
+
+$DataFile->add_measurement($measurement);
+
+$sweep->add_DataFile($DataFile);
+
+$sweep->start();
+
+
+    
