fa5661b75e27542e51354be34508b78604d98c54
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2016-11-04 22:10:27 +0100

    Use one package per source file.

--- Measurement/lib/Lab/MultiChannelInstrument.pm
+++ Measurement/lib/Lab/MultiChannelInstrument.pm
@@ -64,7 +64,7 @@ sub new {
 
         # link shared cache values to the same cache-address in order to keep these parameters up to date for all channels (using a tied Hash):
         my $device_cache;
-        tie %$device_cache, 'DeviceCache', $self;
+        tie %$device_cache, 'Lab::MultiChannelInstrument::DeviceCache', $self;
 
         while ( my ( $k, $v )
             = each %{ $self->{channels}->{$channel}->device_cache() } ) {
@@ -305,39 +305,4 @@ sub device_cache {
     }
 }
 
-package DeviceCache;
-require Tie::Hash;
-use List::MoreUtils qw{ any };
-our $VERSION = '3.530';
-
-our @ISA = 'Tie::ExtraHash';
-
-sub TIEHASH {
-    my $class = shift;
-    my $storage = bless [ {}, @_ ], $class;
-    return $storage;
-}
-
-sub STORE {
-    $_[0][0]{ $_[1] } = $_[2];
-    if ( any { $_[1] eq $_ } @{ $_[0][1]->{multichannel_shared_cache} } ) {
-        $_[0][1]->device_cache( { $_[1] => $_[2] } );
-    }
-}
-
-sub FETCH {
-
-    if ( any { $_[1] eq $_ } @{ $_[0][1]->{multichannel_shared_cache} } ) {
-        return $_[0][1]->device_cache( $_[1] );
-    }
-    else {
-        return $_[0][0]{ $_[1] };
-    }
-}
-
-sub EXISTS {
-    if ( any { $_[1] eq $_ } @{ $_[0][1]->{multichannel_shared_cache} } ) {
-        return exists $_[0][1]->{device_cache}->{ $_[1] };
-    }
-    return exists $_[0][0]->{ $_[1] };
-}
+1;
