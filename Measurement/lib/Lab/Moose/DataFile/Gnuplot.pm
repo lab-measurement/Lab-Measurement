4eca598d8ec94ef337889427d2cdcc8bdffc95ae
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2016-11-17 17:17:02 +0100

    Add docs for Lab::Moose::DataFile::Gnuplot.

--- Measurement/lib/Lab/Moose/DataFile/Gnuplot.pm
+++ Measurement/lib/Lab/Moose/DataFile/Gnuplot.pm
@@ -31,6 +31,48 @@ sub BUILD {
     $self->log_comment( comment => join( "\t", @columns ) );
 }
 
+=head1 NAME
+
+Lab::Moose::DataFile::Gnuplot - Gnuplot style datafile.
+
+=head1 SYNOPSIS
+
+ use Lab::Moose;
+
+ my $folder = datafolder();
+ 
+ my $file = datafile(
+     folder => $folder,
+     filename => 'gnuplot-file.dat',
+     columns => [qw/gate bias current/]
+     );
+
+ $file->log_comment(comment => "some extra comment");
+ $file->log_newline();
+ 
+ $file->log(gate => 1, bias => 2, current => 3);
+
+ $block = Lab::Moose::BlockData->new(...)
+ $file->log_block(
+    prefix => {gate => 1, bias => 2},
+    block => $block
+ );
+
+=head1 METHODS
+
+=head2 new
+
+Requires a 'column' attribute in addition to the L<Lab::Moose::DataFile>
+requirements.
+
+=head2 log
+
+ $file->log(column1 => $value1, column2 => $value2, ...);
+ 
+Log one line of data.
+
+=cut
+
 sub log {
 
     # We do not use MooseX::Params::Validate for performance reasons.
@@ -72,6 +114,21 @@ sub log {
     print {$fh} $line;
 }
 
+=head2 log_block
+
+ $file->log_block(
+     prefix => {column1 => $value1, ...},
+     block => $block,
+     add_newline => 0
+ );
+
+Log a L<Lab::Moose::BlockData> object (i.e a two dimensional matrix).
+You can add prefix columns, which will be the same for each line in the block.
+E.g. when using a spectrum analyzer inside a voltage sweep, one would log the
+returned blockdata object prefixed with the sweep voltage.
+
+=cut
+
 sub log_block {
     my $self = shift;
     my ( $prefix, $block, $add_newline ) = validated_list(
@@ -110,11 +167,28 @@ sub log_block {
     }
 }
 
+=head2 log_newline
+
+ $file->log_newline();
+
+print "\n" to the datafile.
+
+=cut
+
 sub log_newline {
     my $self = shift;
     my $fh   = $self->filehandle;
     print {$fh} "\n";
 }
+
+=head2 log_comment
+
+ $file->log_comment(comment => $string);
+
+log a comment string, which will be prefixed with '#'. If C<$string> contains
+newline characters, several lines of comments will be written.
+
+=cut
 
 sub log_comment {
     my $self = shift;
