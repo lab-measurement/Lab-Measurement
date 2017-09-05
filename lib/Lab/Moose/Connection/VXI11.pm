a180ba60ac114260e082683161577474b799a648
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2017-09-05 11:29:55 +0200

    Add VXI-11 connection backend (untested).

--- lib/Lab/Moose/Connection/VXI11.pm
+++ lib/Lab/Moose/Connection/VXI11.pm
@@ -13,6 +13,16 @@ use Lab::Moose::Instrument qw/timeout_param read_length_param/;
 use Lab::VXI11;
 use namespace::autoclean;
 
+use constant {
+    WAITLOCK_OPERATION_FLAG   => 1 << 0,
+    END_OPERATION_FLAG        => 1 << 3,
+    TERMCHRSET_OPERATION_FLAG => 1 << 7,
+
+    END_REASON    => 1 << 2,
+    CHR_REASON    => 1 << 1,
+    REQCNT_REASON => 1 << 0,
+};
+
 has client => (
     is       => 'ro',
     isa      => 'Lab::VXI11',
@@ -56,8 +66,13 @@ sub BUILD {
         or croak "cannot open VXI-11 connection with $host: $!";
     $self->_client($client);
 
-    my ( $error, $lid, $abortPort, $maxRecvSize )
-        = $client->create_link( 0, 0, 0, $device );
+    my $clientId     = 0;
+    my $lockDevice   = 0;
+    my $lock_timeout = 0;
+    my ( $error, $lid, $abortPort, $maxRecvSize ) = $client->create_link(
+        $client, $lockDevice, $lock_timeout,
+        $device
+    );
     if ($error) {
         croak "Cannot create VXI-11 link. error: $error";
     }
@@ -65,20 +80,94 @@ sub BUILD {
 }
 
 sub Write {
+    my ( $self, %args ) = validated_hash(
+        \@_,
+        timeout_param,
+        command => { isa => 'Str' },
+    );
 
+    my $command = $args{command};
+    my $timeout = $self->_timeout_arg(%args);
+
+    my $client = $self->client();
+    my $lid    = $self->lid();
+
+    my $lock_timeout = 0;
+    my $flags        = END_OPERATION_FLAG;
+
+    my ( $error, $size ) = $client->device_write(
+        $lid, $timeout, $lock_timeout, $flags,
+        $command
+    );
+    if ($error) {
+        croak "VXI-11 device_write failed with error $error.";
+    }
+    my $command_length = length($command);
+    if ( $size != $command_length ) {
+        croak
+            "VXI-11 device_write incomplete: write size: $size, expected: $command_length.";
+    }
 }
 
 sub Read {
+    my ( $self, %args ) = validated_hash(
+        \@_,
+        timeout_param(),
+        read_length_param(),
+    );
+    my $timeout     = $self->_timeout_arg(%args);
+    my $read_length = $self->_read_length_arg(%args);
 
-}
+    my $client = $self->client();
+    my $lid    = $self->lid();
 
-sub Query {
+    my $lock_timeout = 0;
+    my $flags        = 0;
+    my $termChar     = 0;    # not used
 
+    my $result = '';
+
+    while ($read_length) {
+        my ( $error, $reason, $data ) = $client->device_read(
+            $lid,          $read_length, $timeout,
+            $lock_timeout, $flags,       $termChar
+        );
+        if ($error) {
+            croak "VXI-11 device_read failed with error $error.";
+        }
+
+        $result .= $data;
+        $read_length -= length($data);
+
+        if ( $reason & END_REASON or $reason & REQCNT_REASON ) {
+            last;
+        }
+    }
 }
 
 sub Clear {
+    my ( $self, %args ) = validated_hash(
+        \@_,
+        timeout_param(),
+    );
+    my $timeout = $self->_timeout_arg(%args);
 
+    my $client = $self->client();
+    my $lid    = $self->lid();
+
+    my $flags        = 0;
+    my $lock_timeout = 0;
+
+    my ($error)
+        = $client->device_clear( $lid, $flags, $lock_timeout, $timeout );
+    if ($error) {
+        croak "VXI-11 device_clear failed with error $error.";
+    }
 }
+
+#
+# FIXME: DESTROY
+#
 
 with qw/
     Lab::Moose::Connection
@@ -94,15 +183,13 @@ __PACKAGE__->meta->make_immutable();
 
  my $instrument = instrument(
      type => 'random_instrument',
-     connection_type => 'Socket',
-     connection_options => {host => '132.199.11.2', port => 5025},
+     connection_type => 'VXI11',
+     connection_options => {host => '132.199.11.2'}
  );
 
 =head1 DESCRIPTION
 
-This connection uses L<IO::Socket::INET> to interface with the operating
-system's TCP stack. This works on most operating systems without installing any
-additional software.
+Connection backend based on L<Lab::VXI11>.
 
 =cut
 
