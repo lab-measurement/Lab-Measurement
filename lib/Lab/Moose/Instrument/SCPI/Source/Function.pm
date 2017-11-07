b9a1b308562cf3f389b27f6e50ee44740f900a33
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2017-11-07 20:19:10 +0100

    Add SCPI Source roles (get started with YokogawaGS200).
    Source:Function
    Source:Level
    Source:Range

---
+++ lib/Lab/Moose/Instrument/SCPI/Source/Function.pm
@@ -0,0 +1,42 @@
+package Lab::Moose::Instrument::SCPI::Source::Function;
+
+#ABSTRACT: Role for the SCPI SOURce:FUNCtion subsystem
+
+use Moose::Role;
+use Moose::Util::TypeConstraints 'enum';
+use Lab::Moose::Instrument::Cache;
+use Lab::Moose::Instrument qw/validated_getter validated_setter/;
+
+use namespace::autoclean;
+
+=head1 METHODS
+
+=head2 source_function_query
+
+=head2 source_function
+
+ $self->source_function(value => 'VOLT');
+
+Query/Set the type of output signal. Can be 'VOLT' or 'CURR'.
+
+=cut
+
+cache source_function => ( getter => 'source_function_query' );
+
+sub source_function_query {
+    my ( $self, %args ) = validated_getter( \@_ );
+
+    return $self->cached_source_function(
+        $self->query( command => "SOUR:FUNC?", %args ) );
+}
+
+sub source_function {
+    my ( $self, $value, %args ) = validated_setter(
+        \@_,
+        value => { isa => enum( [qw/VOLT CURR/] ) }
+    );
+    $self->write( command => "SOUR:FUNC $value", %args );
+    $self->cached_source_function($value);
+}
+
+1;
