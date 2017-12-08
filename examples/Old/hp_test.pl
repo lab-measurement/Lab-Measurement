0b25e7e7194fbc55538c85b5deeb1101535a4e8f
Andreas K. Huettel <dilfridge@gentoo.org>
2017-12-08 16:32:14 +0100

    Remove some outdated examples

--- examples/Old/hp_test.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-use strict;
-use Lab::Instrument::HP34401A;
-use Time::HiRes qw/usleep/;
-
-my $hp=new Lab::Instrument::HP34401A({GPIB_board=>0,GPIB_address=>21});
-
-print $hp->read_voltage_dc(10,0.0001),"\n";
