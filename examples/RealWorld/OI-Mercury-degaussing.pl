58ce4bdbb9130ce9d29fad03c05bda7480772356
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2018-02-02 10:57:05 +0100

    Fix degaussing script: increase timeout and max_field_deviation.

--- examples/RealWorld/OI-Mercury-degaussing.pl
+++ examples/RealWorld/OI-Mercury-degaussing.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/env perl
 
+use lib '/home/simon/lab-measurement/lib';
+
 use Lab::Moose;
 
 # After Oxford Instruments Technical Note
@@ -16,15 +18,20 @@ use Lab::Moose;
 # They use a start rate of about 2.4 T/min.
 # Our OI Mercury provides a max rate of 1T/min, so we use that as start rate.
 
-my $start_field      = 1;        # Tesla
-my $start_rate       = 1;        # Tesla/min
+my $start_field      = 1;       # Tesla
+my $start_rate       = 1;       # Tesla/min
 my $reduction_factor = 0.8;
-my $stop_field       = 0.001;    # 1mT
+my $stop_field       = 0.01;    # 10mT
 
 my $magnet = instrument(
     type               => 'OI_Mercury::Magnet',
     connection_type    => 'Socket',
-    connection_options => { host => '192.168.3.15' },  # use default port 7020
+    connection_options => { host => '192.168.3.15', timeout => 10 }
+    ,                           # use default port 7020
+
+    # make this large: we do not want to wait long until we saturate at the target points
+    max_field_deviation => 0.01,
+
 );
 
 my $target = $start_field;
@@ -43,3 +50,5 @@ while ( abs($target) > $stop_field ) {
     $target *= -$reduction_factor;
     $rate   *= $reduction_factor;
 }
+
+$magnet->sweep_to_field( target => 0, rate => $rate );
