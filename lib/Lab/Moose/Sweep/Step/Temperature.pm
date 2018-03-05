7ac36adcba60769ff6cc768cc661f4ba744601e9
Simon Reinhardt <simon.reinhardt@stud.uni-regensburg.de>
2018-03-05 16:25:42 +0100

    Add step/list temperature sweep.
    Refactor stabilization/convergence code into Lab::Moose::Stabilizer.

--- lib/Lab/Moose/Sweep/Step/Temperature.pm
+++ lib/Lab/Moose/Sweep/Step/Temperature.pm
@@ -1,60 +1,73 @@
 package Lab::Moose::Sweep::Step::Temperature;
+
 #ABSTRACT: Step/list sweep of temperature
+
+=head1 SYNOPSIS
+
+ my $sweep = sweep(
+     type => 'Step::Temperature',
+     instrument => $oi_triton,
+     from => 0.010, # K
+     to => 0.5,
+     step => 0.05, # K
+
+     # stabilizer options (details in Lab::Moose::Stabilizer)
+     tolerance_setpoint => 0.01,
+     tolerance_std_dev => 0.01,
+     measurement_interval => 1,
+     observation_time => 10,
+     ...
+ );
+
+=head1 Description
+
+Step sweep of temperature. Set temperature with instrument's C<set_T> method.
+
+Use L<Lab::Moose::Stabilizer> to wait until temperature has converged to
+setpoint.
+
+
+=cut
 
 use 5.010;
 use Moose;
 
 extends 'Lab::Moose::Sweep::Step';
 
-has filename_extension => ( is => 'ro', isa => 'Str', default => 'Temp=' );
+has filename_extension => ( is => 'ro', isa => 'Str', default => 'T=' );
 
 has setter => ( is => 'ro', isa => 'CodeRef', builder => '_build_setter' );
-
-sub BUILD {
-    my $self = shift;
-}
+has tolerance_setpoint   => ( is => 'ro', isa => 'Num', default => 0.2 );
+has tolerance_std_dev    => ( is => 'ro', isa => 'Num', default => 0.15 );
+has observation_time     => ( is => 'ro', isa => 'Num', default => 3 * 60 );
+has measurement_interval => ( is => 'ro', isa => 'Num', default => 1 );
+has max_stabilization_time => ( is => 'ro', isa => 'Maybe[Num]' );
 
 sub _build_setter {
     return \&_temp_setter;
 }
 
+sub _getter {
+    my $self = shift;
+    return $self->get_T();
+}
+
 sub _temp_setter {
     my $self  = shift;
     my $value = shift;
-    $self->instrument->sweep_to_temperature( value => $value );
+    $self->instrument->set_T( value => $value );
+    $self->stabilize(
+        setpoint               => $value,
+        getter                 => \&_getter,
+        tolerance_setpoint     => $self->tolerance_setpoint,
+        tolerance_std_dev      => $self->tolerance_std_dev,
+        measurement_interval   => $self->measurement_interval,
+        observation_time       => $self->observation_time,
+        max_stabilization_time => $self->max_stabilization_time,
+        verbose                => 1,
+    );
+
 }
 
 __PACKAGE__->meta->make_immutable();
 1;
-
-__END__
-
-=pod
-
-=head1 SYNOPSIS
-
- my $sweep = sweep(
-     type => 'Step::Temperature',
-     instrument => $triton,
-     from => 0.5, # Kelvin
-     to => 0.015,
-     step => 0.005, # steps of 0.1 Tesla
- );
-
-=head1 Description
-
-Minimalistic (so far) step sweep with following properties:
-
-=over
-
-=item *
-
-Uses instruments C<sweep_to_temperature> method to set the field.
-
-=item *
-
-Default filename extension: C<'Temp='>
-
-=back
-
-=cut
