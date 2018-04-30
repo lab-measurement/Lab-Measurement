69bb07815ff774e171ad3a0de8ab76946c69cbf1
Eugeniy E. Mikhailov <evgmik@gmail.com>
2018-04-30 12:13:20 -0400

    perltidy

--- lib/Lab/Moose/Instrument/SpectrumAnalyzer.pm
+++ lib/Lab/Moose/Instrument/SpectrumAnalyzer.pm
@@ -150,14 +150,17 @@ sub sense_sweep_points_from_traceY_query {
 sub get_Xpoints_number {
     my ( $self, $channel, %args ) = validated_channel_getter( \@_ );
     if ( $self->has_hardwired_number_of_X_points ) {
-	#carp("using hardwired number of points: " . $self->hardwired_number_of_X_points . "\n" );
+
+        #carp("using hardwired number of points: " . $self->hardwired_number_of_X_points . "\n" );
         return $self->cached_sense_sweep_points(
             $self->hardwired_number_of_X_points );
     }
     if ( $self->capable_to_query_number_of_X_points_in_hardware ) {
-	#carp("using hardware capabilities to detect number of points in a sweep\n");
+
+        #carp("using hardware capabilities to detect number of points in a sweep\n");
         return $self->sense_sweep_points_query(%args);
     }
+
     #carp("trying heuristic to detect number of points in a sweep\n");
     return $self->cached_sense_sweep_points(
         $self->sense_sweep_points_from_traceY_query(%args) );
@@ -272,7 +275,7 @@ sub get_NameY {
     my ( $self, %args ) = @_;
     my $name;
     my $unitY = $self->get_UnitY(%args);
-    if ( $unitY =~ qr/dbm|w/i ) { $name = 'Power'; }
+    if    ( $unitY =~ qr/dbm|w/i )       { $name = 'Power'; }
     elsif ( $unitY =~ qr/dbmv|dbuv|w/i ) { $name = 'Amplitude'; }
     else { $name = 'Unknown'; carp( "Unknow Y unit " . $unitY ); }
     return $name;
