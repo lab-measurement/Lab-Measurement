1cbd106eb9403e117fffce0f00a6492156f783d1
Andreas K. Huettel <dilfridge@gentoo.org>
2018-05-16 19:06:49 +0200

    Update POD of the Lab::Measurement module

--- lib/Lab/Measurement.pm
+++ lib/Lab/Measurement.pm
@@ -26,30 +26,36 @@ __END__
 =head1 SYNOPSIS
 
   use Lab::Measurement;
-  
+
+However, by now you probably want to use the following instead:
+
+  use Lab::Moose;
 
 =head1 DESCRIPTION
 
-This module distribution simplifies the task of running a measurement, writing 
-the data to disk and keeping track of necessary meta information that usually 
-later you don't find in your lab book anymore.
+The Lab::Measurement module distribution simplifies the task of running a
+measurement, writing the data to disk and keeping track of necessary meta
+information that usually later you don't find in your lab book anymore.
 
 If your measurements don't come out nice, it's not because you were using the 
 wrong software. 
 
-The entire stack can be loaded by a simple 
-
-  use Lab::Measurement;
-
-command; further required modules will be imported on demand.
+The actual Lab::Measurement module belongs to the deprecated legacy module
+stack. We are in the process of re-writing the entire code base using Modern
+Perl techniques and the Moose object system. Please port your code to the new
+API; its documentation can be found on the Lab::Measurement homepage.
 
 =head1 SEE ALSO
 
 =over 4
 
-=item L<Lab::XPRESS>
+=item L<Lab::Measurement::Manual>
 
-=item L<http://www.labmeasurement.de>
+=item L<Lab::Measurement::Tutorial>
+
+=item L<Lab::Measurement::Roadmap>
+
+=item L<https://www.labmeasurement.de/>
 
 =back
 
