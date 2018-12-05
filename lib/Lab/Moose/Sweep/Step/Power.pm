061845c73c9f38b9a22b4b4ce30a4b3e811021e4
Andreas K. Hüttel <dilfridge@gentoo.org>
2018-12-06 00:46:33 +0100

    Add Power sweep
    Untested but pretty much trivial.
    
    Closes: https://github.com/lab-measurement/Lab-Measurement/issues/27
    Signed-off-by: Andreas K. Hüttel <dilfridge@gentoo.org>

---
+++ lib/Lab/Moose/Sweep/Step/Power.pm
@@ -0,0 +1,47 @@
+package Lab::Moose::Sweep::Step::Power;
+
+#ABSTRACT: Power sweep.
+
+=head1 Description
+
+Step sweep with following properties:
+
+=over
+
+=item *
+
+ Uses instruments C<set_power> method to change the power.
+
+=item *
+
+Default filename extension: C<'Power='>
+
+=back
+
+=cut
+
+use 5.010;
+use Moose;
+
+extends 'Lab::Moose::Sweep::Step';
+
+has filename_extension =>
+    ( is => 'ro', isa => 'Str', default => 'Power=' );
+
+has setter => ( is => 'ro', isa => 'CodeRef', builder => '_build_setter' );
+
+has instrument =>
+    ( is => 'ro', isa => 'Lab::Moose::Instrument', required => 1 );
+
+sub _build_setter {
+    return \&_power_setter;
+}
+
+sub _power_setter {
+    my $self  = shift;
+    my $value = shift;
+    $self->instrument->set_power( value => $value );
+}
+
+__PACKAGE__->meta->make_immutable();
+1;
