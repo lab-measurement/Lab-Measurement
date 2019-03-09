5e8f550c80c0ac8194be06eda4f05428ebe3b7d9
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2019-03-09 14:51:29 +0100

    Add general level sweep XPRESS::Sweep::Level.
    XPRESS::Sweep::Voltage cannot be used to sweep current output of Yokos.

--- t/update-ids.pl
+++ t/update-ids.pl
@@ -4,29 +4,15 @@ use warnings;
 use strict;
 use Getopt::Long qw/:config gnu_getopt/;
 
-my $start_id = 0;
-my $diff;
-
-GetOptions(
-    "start|s=i" => \$start_id,
-    "diff|d=i"  => \$diff,
-) or die "GetOptions";
-
 my $filename = $ARGV[0] // die "Need filename arg";
-if ( not defined $diff ) {
-    die "Need diff arg";
-}
 
 open my $fh, '<', $filename
     or die "Cannot open $filename: $!";
-
+my $id = 0;
 while ( my $line = <$fh> ) {
-    if ( $line =~ /id: ([0-9]+)/ ) {
-        my $id = $1;
-        if ( $id >= $start_id ) {
-            $id += $diff;
-            $line =~ s/id: [0-9]+/id: $id/;
-        }
+    if ( $line =~ /id: [0-9]+/ ) {
+        $line =~ s/id: [0-9]+/id: $id/;
+        ++$id;
     }
     print $line;
 }
