6a41441c3d37467db5eaf00aec9f61926450410e
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2020-02-14 16:46:59 +0100

    Lakeshore 340: add more functions

--- lib/Lab/Moose/Instrument/Lakeshore340.pm
+++ lib/Lab/Moose/Instrument/Lakeshore340.pm
@@ -32,6 +32,9 @@ sub BUILD {
     $self->cls();
 }
 
+my %channel_arg = ( channel => { isa => enum( [qw/A B/] ), optional => 1 } );
+my %loop_arg = ( loop => { isa => enum( [qw/1 2/] ) } );
+
 =encoding utf8
 
 =head1 SYNOPSIS
@@ -69,7 +72,7 @@ alias for C<get_T>.
 sub get_T {
     my ( $self, %args ) = validated_getter(
         \@_,
-        channel => { isa => enum( [qw/A B/] ), optional => 1 }
+        %channel_arg
     );
     my $channel = delete $args{channel} // $self->input_channel();
     return $self->query( command => "KRDG? $channel", %args );
@@ -78,6 +81,169 @@ sub get_T {
 sub get_value {
     my $self = shift;
     return $self->get_T(@_);
+}
+
+=head2 set_setpoint/get_setpoint
+ # set/get SP for loop 1 in whatever units the setpoint is using
+ $lakeshore->set_setpoint(value => 10, loop => 1); 
+ my $setpoint1 = $lakeshore->get_setpoint(loop => 1);
+ 
+=cut
+
+sub get_setpoint {
+    my ( $self, %args ) = validated_getter(
+        \@_,
+        %loop_arg
+    );
+    my $loop = delete $args{loop};
+    return $self->query( command => "SETP? $loop", %args );
+}
+
+sub set_setpoint {
+    my ( $self, $value, %args ) = validated_setter(
+        \@_,
+        %loop_arg
+    );
+    my $loop = delete $args{loop};
+
+    # Device bug. The 340 cannot parse values with too many digits.
+    $value = sprintf( "%.6G", $value );
+    $self->write( command => "SETP $loop,$value", %args );
+}
+
+=head2 set_T
+
+alias for C<set_setpoint>
+
+=cut
+
+sub set_T {
+    my $self = shift;
+    $self->set_setpoint(@_);
+}
+
+=head2 set_heater_range/get_heater_range
+
+ $lakeshore->set_heater_range(value => 1);
+ my $range = $lakeshore->get_heater_range();
+
+Value is one of 0 (OFF),1,...,5 (MAX)
+
+=cut
+
+sub set_heater_range {
+    my ( $self, $value, %args ) = validated_setter(
+        \@_,
+        value => { isa => enum( [qw/0 1 2 3 4 5/] ) }
+    );
+    $self->write( command => "RANGE $value", %args );
+}
+
+sub get_heater_range {
+    my ( $self, %args ) = validated_getter( \@_ );
+    return $self->query( command => "RANGE?", %args );
+}
+
+=head2 set_control_mode/get_control_mode
+
+Specifies the control mode. Valid entries: 1 = Manual PID, 2 = Zone,
+ 3 = Open Loop 4 = AutoTune PID, 5 = AutoTune PI, 6 = AutoTune P.
+
+ # Set loop 1 to manual PID
+ $lakeshore->set_control_mode(value => 1, loop => 1);
+ my $cmode = $lakeshore->get_control_mode(loop => 1);
+
+=cut
+
+sub set_control_mode {
+    my ( $self, $value, %args ) = validated_setter(
+        \@_,
+        value => { isa => enum( [ ( 1 .. 6 ) ] ) },
+        %loop_arg
+    );
+    my $loop = delete $args{loop};
+    return $self->write( command => "CMODE $loop,$value", %args );
+}
+
+sub get_control_mode {
+    my ( $self, %args ) = validated_getter(
+        \@_,
+        %loop_arg
+    );
+    my $loop = delete $args{loop};
+    return $self->query( command => "CMODE? $loop", %args );
+}
+
+=head2 set_control_parameters/get_control_parameters
+
+ $lakeshore->set_control_parameters(
+    loop => 1,
+    input => 'A',
+    units => 1, # 1 = Kelvin, 2 = Celsius, 3 = sensor units
+    state => 1, # 0 = off, 1 = on
+    powerup_enable => 1, # 0 = off, 1 = on, optional with default = off
+ );
+ my %args = $lakeshore->get_control_parameters(loop => 1);
+
+=cut
+
+sub set_control_parameters {
+    my ( $self, %args ) = validated_getter(
+        \@_,
+        %loop_arg,
+        %channel_arg,
+        units          => { isa => enum( [qw/1 2 3/] ) },
+        state          => { isa => enum( [qw/0 1/] ) },
+        powerup_enable => { isa => enum( [qw/0 1/] ), default => 1 },
+    );
+    my $loop           = delete $args{loop};
+    my $channel        = delete $args{channel} // $self->input_channel();
+    my $units          = delete $args{units};
+    my $state          = delete $args{state};
+    my $powerup_enable = delete $args{powerup_enable};
+    $self->write( command => "CSET $loop, $channel, $units, $state,"
+            . "$powerup_enable", %args );
+}
+
+sub get_control_parameters {
+    my ( $self, %args ) = validated_getter(
+        \@_,
+        %loop_arg
+    );
+    my $loop = delete $args{loop};
+    my $rv   = $self->query( command => "CSET? $loop", %args );
+    my @rv   = split /,/, $rv;
+    return (
+        channel        => $rv[0], units => $rv[1], state => $rv[2],
+        powerup_enable => $rv[3]
+    );
+}
+
+=head2 set_input_curve/get_input_curve
+
+ # Set channel 'B' to use curve 25
+ $lakeshore->set_input_curve(channel => 'B', value => 25);
+ my $curve = $lakeshore->get_input_curve(channel => 'B');
+
+=cut
+
+sub set_input_curve {
+    my ( $self, $value, %args ) = validated_setter(
+        \@_,
+        %channel_arg,
+        value => { isa => enum( [ 0 .. 60 ] ) },
+    );
+    my $channel = delete $args{channel} // $self->input_channel();
+    $self->write( command => "INCRV $channel,$value", %args );
+}
+
+sub get_input_curve {
+    my ( $self, %args ) = validated_getter(
+        \@_,
+        %channel_arg,
+    );
+    my $channel = delete $args{channel} // $self->input_channel();
+    return $self->query( command => "INCRV $channel", %args );
 }
 
 =head2 Consumed Roles
