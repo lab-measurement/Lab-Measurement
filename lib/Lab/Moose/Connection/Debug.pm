f2dcd5138461c121f2e151a7991adc95d3e62352
Andreas K. Hüttel <dilfridge@gentoo.org>
2020-06-27 18:55:50 +0200

    pod documentation

--- lib/Lab/Moose/Connection/Debug.pm
+++ lib/Lab/Moose/Connection/Debug.pm
@@ -1,6 +1,6 @@
 package Lab::Moose::Connection::Debug;
 
-#ABSTRACT: Debug connection
+#ABSTRACT: Debug connection, printing / reading on terminal
 
 use Moose;
 use 5.010;
@@ -15,46 +15,6 @@ has verbose => (
     isa     => 'Bool',
     default => 1,
 );
-
-sub Write {
-    my $self = shift;
-    my %args = @_;
-    if ( $self->verbose() ) {
-        carp "Write called with args:\n", Dump \%args, "\n";
-    }
-}
-
-sub Read {
-    my $self = shift;
-    my %args = @_;
-    carp "Read called with args:\n", Dump \%args, "\n";
-    say "enter return value:";
-    my $retval = <STDIN>;
-    chomp $retval;
-    return $retval;
-}
-
-sub Query {
-    my $self = shift;
-    my %args = @_;
-    carp "Query called with args:\n", Dump \%args, "\n";
-    say "enter return value:";
-    my $retval = <STDIN>;
-    chomp $retval;
-    return $retval;
-}
-
-sub Clear {
-    my $self = shift;
-    if ( $self->verbose ) {
-        carp "Clear called";
-    }
-}
-
-with 'Lab::Moose::Connection';
-
-__PACKAGE__->meta->make_immutable();
-1;
 
 =head1 SYNOPSIS
 
@@ -74,4 +34,72 @@ __PACKAGE__->meta->make_immutable();
 Debug connection object. Print out C<Write> commands and prompt answer for
 C<Read> commands.
 
+=head1 METHODS
+
+=head2 Write
+
+If the connection option verbose is set, output the content of all write
+commands to the terminal. Otherwise, do nothing.
+
 =cut
+
+sub Write {
+    my $self = shift;
+    my %args = @_;
+    if ( $self->verbose() ) {
+        carp "Write called with args:\n", Dump \%args, "\n";
+    }
+}
+
+=head2 Read
+
+Output the arguments of the read command to the terminal, and request
+a response there, which is given as result of the read.
+
+=cut
+
+sub Read {
+    my $self = shift;
+    my %args = @_;
+    carp "Read called with args:\n", Dump \%args, "\n";
+    say "enter return value:";
+    my $retval = <STDIN>;
+    chomp $retval;
+    return $retval;
+}
+
+=head2 Query
+
+Output the arguments of the query command to the terminal, and request
+a response there, which is given as result of the query.
+
+=cut
+
+sub Query {
+    my $self = shift;
+    my %args = @_;
+    carp "Query called with args:\n", Dump \%args, "\n";
+    say "enter return value:";
+    my $retval = <STDIN>;
+    chomp $retval;
+    return $retval;
+}
+
+=head2 Clear
+
+Output "Clear called" on the terminal.
+
+=cut
+
+sub Clear {
+    my $self = shift;
+    if ( $self->verbose ) {
+        carp "Clear called";
+    }
+}
+
+with 'Lab::Moose::Connection';
+
+__PACKAGE__->meta->make_immutable();
+1;
+
--- lib/Lab/Moose/Connection/Mock.pm
+++ lib/Lab/Moose/Connection/Mock.pm
@@ -1,6 +1,6 @@
 package Lab::Moose::Connection::Mock;
 
-#ABSTRACT: Mock connection
+#ABSTRACT: Mock connection, supplying instrument responses from a log file
 
 use 5.010;
 use Moose;
@@ -9,6 +9,23 @@ use namespace::autoclean;
 use Data::Dumper;
 use YAML::XS;
 use Carp;
+
+=head1 SYNOPSIS
+
+ use Lab::Moose;
+
+ my $instrument = instrument(
+    type => 'some_instrument',
+    connection_type => 'Mock',
+    connection_options => { log_file => 'log.yml' }, # or log_fh => $fh,
+ );
+
+=head1 DESCRIPTION
+
+Mock connection object for unit testing. Uses a log recorded previously
+with a real instrument using L<Lab::Moose::Instrument::Log>.
+
+=cut
 
 has log_file => (
     is        => 'ro',
@@ -109,19 +126,3 @@ with 'Lab::Moose::Connection';
 $meta->make_immutable();
 1;
 
-=head1 SYNOPSIS
-
- use Lab::Moose;
-
- my $instrument = instrument(
-    type => 'some_instrument',
-    connection_type => 'Mock',
-    connection_options => { log_file => 'log.yml' }, # or log_fh => $fh,
- );
-
-=head1 DESCRIPTION
-
-Mock connection object for unit testing. Uses previously recorded log recorded
-with a real instrument using L<Lab::Instrument::Log>.
-
-=cut
