f64d6cea785bbc08f35960eea36fc4464ae88077
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2020-12-21 13:03:16 +0100

    RS_ZNL: fix format border command

--- lib/Lab/Moose/Instrument/RS_ZNL.pm
+++ lib/Lab/Moose/Instrument/RS_ZNL.pm
@@ -3,7 +3,7 @@ package Lab::Moose::Instrument::RS_ZNL;
 #ABSTRACT: Rohde & Schwarz ZNL Vector Network Analyzer
 
 use v5.20;
-
+use Carp 'croak';
 use Moose;
 
 extends 'Lab::Moose::Instrument::RS_ZVA';
@@ -22,6 +22,20 @@ See L<Lab::Moose::Instrument::VNASweep> for the high-level C<sparam_sweep> and
 C<sparam_catalog> methods.
 
 =cut
+
+# The ZNL only supports SWAP byte order. It does not have a FORMAT:BORDER command.
+sub format_border_query() {
+    return 'SWAP';
+}
+
+sub format_border {
+    my ( $self, $value, %args ) = validated_setter( \@_ );
+
+    if ( $value ne 'SWAP' ) {
+        croak 'The R&S ZNL only suppots SWAP byte order.';
+    }
+    return $self->cached_format_border($value);
+}
 
 __PACKAGE__->meta->make_immutable();
 
