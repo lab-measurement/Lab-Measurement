02c99f4ff241800a11db42587ef5eca13bddd908
Simon Reinhardt <simon.reinhardt@physik.uni-r.de>
2021-07-01 23:08:45 +0200

    github action: try build with Ubuntu's gnuplot package

--- .github/scripts/prove-test.bash
+++ .github/scripts/prove-test.bash
@@ -9,19 +9,19 @@ perl --version
 # install gnuplot version specified by environment variable $gp_version
 #
 
-cd /tmp
-wget https://github.com/lab-measurement/Lab-Measurement-Homepage/raw/master/gnuplot-5.2.4.tar.gz
-tar -xf gnuplot-$gp_version.tar.gz
-cd gnuplot-$gp_version
-./configure --prefix=$HOME/local
-make
-make install
-export PATH="$HOME/local/bin:$PATH"
-gnuplot --version 
-# Sometimes Alien::Gnuplot does not find the installed gnuplot
-# (but problem not reproducible, see
-# https://travis-ci.org/lab-measurement/Lab-Measurement/jobs/378743671
-export GNUPLOT_BINARY=$HOME/local/bin/gnuplot
+# cd /tmp
+# wget https://github.com/lab-measurement/Lab-Measurement-Homepage/raw/master/gnuplot-5.2.4.tar.gz
+# tar -xf gnuplot-$gp_version.tar.gz
+# cd gnuplot-$gp_version
+# ./configure --prefix=$HOME/local
+# make
+# make install
+# export PATH="$HOME/local/bin:$PATH"
+# gnuplot --version 
+# # Sometimes Alien::Gnuplot does not find the installed gnuplot
+# # (but problem not reproducible, see
+# # https://travis-ci.org/lab-measurement/Lab-Measurement/jobs/378743671
+# export GNUPLOT_BINARY=$HOME/local/bin/gnuplot
 
 
 #
