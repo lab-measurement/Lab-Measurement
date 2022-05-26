423d2eb0f51b5775e52b7c8bd5ed5e64a6938c09
Erik Fabrizzi <erikfabrizzi99@gmail.com>
2022-05-26 20:11:57 +0200

    3DSwp Implementation checkpoint

--- lib/Lab/Moose/Instrument/NanonisTremea.pm
+++ lib/Lab/Moose/Instrument/NanonisTremea.pm
@@ -100,13 +100,20 @@ sub intArrayUnpacker {
     push @intArray, $int;
     $position+=4;
   }
-  print(join(",",@intArray));
-  #return @intArray;
+
+  return @intArray;
 }
+
+
+
 
 
 =head1 1DSweep
 =cut
+
+
+
+
 
 
 sub oneDSwp_AcqChsSet { 
@@ -150,6 +157,7 @@ sub oneDSwp_SwpSignalSet {
     $strlen=nt_int($strlen);
     my $head= $self->nt_header($command_name,$bodysize,0);
     $self->write(command=>$head.$strlen.$channelName);
+    sleep(1);
 }
 
 sub oneDSwp_SwpSignalGet {
@@ -323,8 +331,558 @@ sub oneDSwp_Open {
 
 
 
+
+
+
+=head1 3DSwp
+=cut 
+
+
+
+sub threeDSwp_AcqChsSet {
+  my $self = shift;
+  my $Number_of_Channels= shift;
+  my $command_name = "3dswp.acqchsset";
+  my $bodysize = 4*($Number_of_Channels+1);
+  my $header = $self->nt_header($command_name,$bodysize,0);
+  my $body = nt_int($Number_of_Channels);
+  while(scalar(@_)!=0){
+    $body =$body.nt_int(shift);
+  }
+  $self->write(command=>$header.$body);
+}
+
+sub threeDSwp_AcqChsGet {
+
+  my $self = shift;
+  my $command_name = "3dswp.acqchsget";
+  $self->write(command=>$self->nt_header($command_name,0,1));
+
+  my $return= $self->binary_read();
+  my $Number_of_Channels = unpack("N!", substr($return,40,4));
+  my $pos = 4*($Number_of_Channels);
+  my @indexes = $self->intArrayUnpacker($Number_of_Channels,substr($return,44,$pos));
+  $pos +=44;
+  my $Channel_names_size = unpack("N!",substr($return,$pos,4));
+  my $Channel_num = unpack("N!",substr($return,$pos+4,4));
+  my %names = $self->strArrayUnpacker($Channel_num,substr($return,$pos+8,$Channel_names_size));
+  my %combined;
+
+  for(my $idx = 0; $idx<scalar(@indexes);$idx++){
+      $combined{$indexes[$idx]}=$names{$idx} 
+  }
+  return(%combined);
+}
+
+sub threeDSwp_SaveOptionsSet {
+    my $self = shift ;
+    my $Series_Name = shift;
+    my $DT_Folder_opt = shift;
+    my $Comment = shift;
+    my @Module_Names = @_ ;
+
+    print(join(",",@Module_Names)."\n");
+
+}
+
+sub threeDSwp_Start {
+  my $self = shift;
+  my $command_name= "3dswp.start";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head);
+}
+
+sub threeDSwp_Stop {
+  my $self = shift;
+  my $command_name= "3dswp.stop";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head);
+}
+
+sub threeDSwp_Open {
+  my $self = shift;
+  my $command_name= "3dswp.open";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head);
+}
+
+sub threeDSwp_StatusGet {
+  my $self = shift;
+  my $command_name= "3dswp.statusget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Status= substr $return,40,4;
+  $Status= unpack("N",$Status);
+  return($Status);
+}
+
+=head2 3DSwp.SwpChannel
+=cut
+
+sub threeDSwp_SwpChSignalSet {
+  my $self = shift;
+  my $Sweep_channel_index = shift;
+  my $command_name= "3dswp.swpchsignalset";
+  my $bodysize = 4;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head.nt_int($Sweep_channel_index));
+}
+
+sub threeDSwp_SwpChLimitsSet {
+  my $self = shift;
+  my ($Start,$Stop)= @_;
+  my $command_name= "3dswp.swpchlimitsset";
+  my $bodysize = 8;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_float32($Start);
+  $body=$body.nt_float32($Stop);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_SwpChLimitsGet {
+  my $self = shift;
+  my $command_name= "3dswp.swpchlimitsget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Start= substr $return,40,4;
+  $Start= unpack("f>",$Start);
+  my $Stop= substr $return,44,4;
+  $Stop= unpack("f>",$Stop);
+  return($Start,$Stop);
+}
+
+sub threeDSwp_SwpChPropsSet {
+  my $self = shift;
+  my ($Number_of_points,$Number_of_sweeps,$Backward_sweep,$End_of_sweep_action,$End_of_sweep_arbitrary_value,$Save_all)= @_;
+  my $command_name= "3dswp.swpchpropsset";
+  my $bodysize = 24;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_int($Number_of_points);
+  $body=$body.nt_int($Number_of_sweeps);
+  $body=$body.nt_int($Backward_sweep);
+  $body=$body.nt_int($End_of_sweep_action);
+  $body=$body.nt_float32($End_of_sweep_arbitrary_value);
+  $body=$body.nt_int($Save_all);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_SwpChPropsGet {
+  my $self = shift;
+  my $command_name= "3dswp.swpchpropsget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Number_of_points= substr $return,40,4;
+  $Number_of_points= unpack("N!",$Number_of_points);
+  my $Number_of_sweeps= substr $return,44,4;
+  $Number_of_sweeps= unpack("N!",$Number_of_sweeps);
+  my $Backward_sweep= substr $return,48,4;
+  $Backward_sweep= unpack("N",$Backward_sweep);
+  my $End_of_sweep_action= substr $return,52,4;
+  $End_of_sweep_action= unpack("N",$End_of_sweep_action);
+  my $End_of_sweep_arbitrary_value= substr $return,56,4;
+  $End_of_sweep_arbitrary_value= unpack("f>",$End_of_sweep_arbitrary_value);
+  my $Save_all= substr $return,60,4;
+  $Save_all= unpack("N",$Save_all);
+  return($Number_of_points,$Number_of_sweeps,$Backward_sweep,$End_of_sweep_action,$End_of_sweep_arbitrary_value,$Save_all);
+}
+
+sub threeDSwp_SwpChTimingSet {
+  my $self = shift;
+  my ($Initial_settling_time_s,$Settling_time_s,$Integration_time_s,$End_settling_time_s,$Maximum_slw_rate)= @_;
+  my $command_name= "3dswp.swpchtimingset";
+  my $bodysize = 20;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_float32($Initial_settling_time_s);
+  $body=$body.nt_float32($Settling_time_s);
+  $body=$body.nt_float32($Integration_time_s);
+  $body=$body.nt_float32($End_settling_time_s);
+  $body=$body.nt_float32($Maximum_slw_rate);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_SwpChTimingGet {
+  my $self = shift;
+  my $command_name= "3dswp.swpchtimingget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Initial_settling_time_s= substr $return,40,4;
+  $Initial_settling_time_s= unpack("f>",$Initial_settling_time_s);
+  my $Settling_time_s= substr $return,44,4;
+  $Settling_time_s= unpack("f>",$Settling_time_s);
+  my $Integration_time_s= substr $return,48,4;
+  $Integration_time_s= unpack("f>",$Integration_time_s);
+  my $End_settling_time_s= substr $return,52,4;
+  $End_settling_time_s= unpack("f>",$End_settling_time_s);
+  my $Maximum_slw_rate= substr $return,56,4;
+  $Maximum_slw_rate= unpack("f>",$Maximum_slw_rate);
+  return($Initial_settling_time_s,$Settling_time_s,$Integration_time_s,$End_settling_time_s,$Maximum_slw_rate);
+}
+
+sub threeDSwp_SwpChModeSet {
+  my $self = shift;
+  my $Channel_name = shift;
+  my $name_len = length($Channel_name);
+  my $bodysize = 4*($name_len+1);
+  my $command_name = "3dswp.swpchmodeset";
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head.nt_int($name_len).$Channel_name);
+}
+
+sub threeDSwp_SwpChModeGet {
+  my $self = shift;
+  my $command_name = "3dswp.swpchmodeget";
+  my $head = $self->nt_header($command_name,0,1);
+  $self->write(command=>$head);
+  my $return= $self->binary_read();
+  return substr($return,44,unpack("N!",substr($return,40,4)));
+}
+
+=head2 3DSwp.StepChanne1
+=cut
+
+sub threeDSwp_StpCh1SignalSet {
+  my $self = shift;
+  my $Step_channel_1_index = shift;
+  my $command_name= "3dswp.stpch1signalset";
+  my $bodysize = 4;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head.nt_int($Step_channel_1_index));
+}
+
+sub threeDSwp_StpCh1LimitsSet {
+  my $self = shift;
+  my ($Start,$Stop)= @_;
+  my $command_name= "3dswp.stpch1limitsset";
+  my $bodysize = 8;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_float32($Start);
+  $body=$body.nt_float32($Stop);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_StpCh1LimitsGet {
+  my $self = shift;
+  my $command_name= "3dswp.stpch1limitsget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Start= substr $return,40,4;
+  $Start= unpack("f>",$Start);
+  my $Stop= substr $return,44,4;
+  $Stop= unpack("f>",$Stop);
+  return($Start,$Stop);
+}
+
+sub threeDSwp_StpCh1PropsSet {
+  my $self = shift;
+  my ($Number_of_points,$Backward_sweep,$End_of_sweep_action,$End_of_sweep_arbitrary_value)= @_;
+  my $command_name= "3dswp.stpch1propsset";
+  my $bodysize = 16;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_int($Number_of_points);
+  $body=$body.nt_int($Backward_sweep);
+  $body=$body.nt_int($End_of_sweep_action);
+  $body=$body.nt_float32($End_of_sweep_arbitrary_value);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_StpCh1PropsGet {
+  my $self = shift;
+  my $command_name= "3dswp.stpch1propsget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Number_of_points= substr $return,40,4;
+  $Number_of_points= unpack("N!",$Number_of_points);
+  my $Backward_sweep= substr $return,44,4;
+  $Backward_sweep= unpack("N",$Backward_sweep);
+  my $End_of_sweep_action= substr $return,48,4;
+  $End_of_sweep_action= unpack("N",$End_of_sweep_action);
+  my $End_of_sweep_arbitrary_value= substr $return,52,4;
+  $End_of_sweep_arbitrary_value= unpack("f>",$End_of_sweep_arbitrary_value);
+  return($Number_of_points,$Backward_sweep,$End_of_sweep_action,$End_of_sweep_arbitrary_value);
+}
+
+sub threeDSwp_StpCh1TimingSet {
+  my $self = shift;
+  my ($Initial_settling_time_s,$End_settling_time_s,$Maximum_slw_rate)= @_;
+  my $command_name= "3dswp.stpch1timingset";
+  my $bodysize = 12;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_float32($Initial_settling_time_s);
+  $body=$body.nt_float32($End_settling_time_s);
+  $body=$body.nt_float32($Maximum_slw_rate);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_StpCh1TimingGet {
+  my $self = shift;
+  my $command_name= "3dswp.stpch1timingget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Initial_settling_time_s= substr $return,40,4;
+  $Initial_settling_time_s= unpack("f>",$Initial_settling_time_s);
+  my $End_settling_time_s= substr $return,44,4;
+  $End_settling_time_s= unpack("f>",$End_settling_time_s);
+  my $Maximum_slw_rate= substr $return,48,4;
+  $Maximum_slw_rate= unpack("f>",$Maximum_slw_rate);
+  return($Initial_settling_time_s,$End_settling_time_s,$Maximum_slw_rate);
+}
+
+=head2 3DSwp.StepChannel2
+=cut
+
+sub threeDSwp_StpCh2SignalSet {
+  my $self = shift;
+  my $Channel_name = shift;
+  my $name_len = length($Channel_name);
+  my $bodysize = 4*($name_len+1);
+  my $command_name = "3dswp.stpch2signalset";
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head.nt_int($name_len).$Channel_name);
+}
+
+sub threeDSwp_StpCh2SignalGet {
+  my $self = shift;
+  my $command_name = "3dswp.stpch2signalget";
+  my $head = $self->nt_header($command_name,0,1);
+  $self->write(command=>$head);
+  my $return= $self->binary_read();
+  return substr($return,44,unpack("N!",substr($return,40,4)));
+}
+
+sub threeDSwp_StpCh2LimitsSet {
+  my $self = shift;
+  my ($Start,$Stop)= @_;
+  my $command_name= "3dswp.stpch2limitsset";
+  my $bodysize = 8;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_float32($Start);
+  $body=$body.nt_float32($Stop);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_StpCh2LimitsGet {
+  my $self = shift;
+  my $command_name= "3dswp.stpch2limitsget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Start= substr $return,40,4;
+  $Start= unpack("f>",$Start);
+  my $Stop= substr $return,44,4;
+  $Stop= unpack("f>",$Stop);
+  return($Start,$Stop);
+}
+
+sub threeDSwp_StpCh2PropsSet {
+  my $self = shift;
+  my ($Number_of_points,$Backward_sweep,$End_of_sweep_action,$End_of_sweep_arbitrary_value)= @_;
+  my $command_name= "3dswp.stpch2propsset";
+  my $bodysize = 16;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_int($Number_of_points);
+  $body=$body.nt_int($Backward_sweep);
+  $body=$body.nt_int($End_of_sweep_action);
+  $body=$body.nt_float32($End_of_sweep_arbitrary_value);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_StpCh2PropsGet {
+  my $self = shift;
+  my $command_name= "3dswp.stpch2propsget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Number_of_points= substr $return,40,4;
+  $Number_of_points= unpack("N!",$Number_of_points);
+  my $Backward_sweep= substr $return,44,4;
+  $Backward_sweep= unpack("N",$Backward_sweep);
+  my $End_of_sweep_action= substr $return,48,4;
+  $End_of_sweep_action= unpack("N",$End_of_sweep_action);
+  my $End_of_sweep_arbitrary_value= substr $return,52,4;
+  $End_of_sweep_arbitrary_value= unpack("f>",$End_of_sweep_arbitrary_value);
+  return($Number_of_points,$Backward_sweep,$End_of_sweep_action,$End_of_sweep_arbitrary_value);
+}
+
+sub threeDSwp_StpCh2TimingSet {
+  my $self = shift;
+  my ($Initial_settling_time_s,$End_settling_time_s,$Maximum_slw_rate)= @_;
+  my $command_name= "3dswp.stpch2timingset";
+  my $bodysize = 12;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_float32($Initial_settling_time_s);
+  $body=$body.nt_float32($End_settling_time_s);
+  $body=$body.nt_float32($Maximum_slw_rate);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_StpCh2TimingGet {
+  my $self = shift;
+  my $command_name= "3dswp.stpch2timingget";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head);
+  my $return = $self->binary_read(); 
+  my $Initial_settling_time_s= substr $return,40,4;
+  $Initial_settling_time_s= unpack("f>",$Initial_settling_time_s);
+  my $End_settling_time_s= substr $return,44,4;
+  $End_settling_time_s= unpack("f>",$End_settling_time_s);
+  my $Maximum_slw_rate= substr $return,48,4;
+  $Maximum_slw_rate= unpack("f>",$Maximum_slw_rate);
+  return($Initial_settling_time_s,$End_settling_time_s,$Maximum_slw_rate);
+}
+
+=head2 3DSwp.Timing
+=cut
+
+sub threeDSwp_TimingRowLimitSet {
+  #Everythinbg seems set properlly, wierd Behavior on nanonis Software side.
+  my $self = shift;
+  my ($Row_index,$Maximum_time_seconds,$Channel_index)= @_;
+  my $command_name= "3dswp.timingrowlimitset";
+  my $bodysize = 12;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_int($Row_index);
+  $body=$body.nt_int($Maximum_time_seconds); #Gets translated to int software side..I don't know,should be f32.
+  $body=$body.nt_int($Channel_index);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_TimingRowLimitGet {
+  my $self = shift;
+  my $Row_index = shift;
+  my $command_name= "3dswp.timingrowlimitget";
+  my $bodysize = 4;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  $self->write(command=>$head.nt_int($Row_index));
+
+  my $return = $self->binary_read();
+  my $Maximum_time= unpack("f>",substr($return,40,4));
+  my $Channel_index = unpack("N!",substr($return,44,4));
+  my $Channel_names_size = unpack("N!",substr($return,48,4));
+  my $Channel_num= unpack("N!",substr($return,52,4));
+  my $strArray = substr $return,56,$Channel_names_size;
+  my %channels = $self->strArrayUnpacker($Channel_num,$strArray);
+
+  return($Maximum_time,$Channel_index,%channels);
+}
+
+sub threeDSwp_TimingRowMethodsSet {
+  my $self = shift;
+  my ($Row_index,$Method_lower,$Method_middle,$Method_upper,$Method_alternative)= @_;
+  my $command_name= "3dswp.timingrowmethodsset";
+  my $bodysize = 20;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_int($Row_index);
+  $body=$body.nt_int($Method_lower);
+  $body=$body.nt_int($Method_middle);
+  $body=$body.nt_int($Method_upper);
+  $body=$body.nt_int($Method_alternative);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_TimingRowMethodsGet {
+  my $self = shift;
+  my ($Row_index)= @_;
+  my $command_name= "3dswp.timingrowmethodsget";
+  my $bodysize = 4;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  my $body=nt_int($Row_index);
+  $self->write(command=>$head.$body);
+  my $return = $self->binary_read(); 
+  my $Method_lower= substr $return,40,4;
+  $Method_lower= unpack("N!",$Method_lower);
+  my $Method_middle= substr $return,44,4;
+  $Method_middle= unpack("N!",$Method_middle);
+  my $Method_upper= substr $return,48,4;
+  $Method_upper= unpack("N!",$Method_upper);
+  my $Method_alternative= substr $return,52,4;
+  $Method_alternative= unpack("N!",$Method_alternative);
+  return($Method_lower,$Method_middle,$Method_upper,$Method_alternative);
+}
+
+
+sub threeDSwp_TimingRowValsSet {
+  #The documentation indicates that this function requires float32 input but actually gets f64 
+  my $self = shift;
+  my ($Row_index,$MR_from,$LR_value,$MR_value,$MR_to,$UR_value,$AR_value)= @_;
+  my $command_name= "3dswp.timingrowvalsset";
+  my $bodysize = 52;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_int($Row_index);
+  $body=$body.nt_float64($MR_from);
+  $body=$body.nt_float64($LR_value);
+  $body=$body.nt_float64($MR_value);
+  $body=$body.nt_float64($MR_to);
+  $body=$body.nt_float64($UR_value);
+  $body=$body.nt_float64($AR_value);
+  $self->write(command=>$head.$body);
+  # my $return = $self->binary_read();
+  # print($return."\n");
+}
+
+sub threeDSwp_TimingRowValsGet {
+  #The documentation indicates that this function returns float32 but actually returns f64 
+  my $self = shift;
+  my ($Row_index)= @_;
+  my $command_name= "3dswp.timingrowvalsget";
+  my $bodysize = 4;
+  my $head= $self->nt_header($command_name,$bodysize,1);
+  my $body=nt_int($Row_index);
+  $self->write(command=>$head.$body);
+  my $return = $self->binary_read(); 
+  my $MR_from= unpack("d>",substr($return,40,8));
+  my $LR_value = unpack("d>",substr($return,48,8));
+  my $MR_value = unpack("d>",substr($return,56,8));
+  my $MR_to = unpack("d>",substr($return,64,8));
+  my $UR_value = unpack("d>",substr($return,72,8));
+  my $AR_value = unpack("d>",substr($return,80,8));
+  print(unpack("d>",substr($return,88,8))."\n");
+  return($MR_from,$LR_value,$MR_value,$MR_to,$UR_value,$AR_value);
+
+}
+
+sub threeDSwp_TimingEnable {
+  my $self = shift;
+  my ($Enable)= @_;
+  my $command_name= "3dswp.timingenable";
+  my $bodysize = 4;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  my $body=nt_uint32($Enable);
+  $self->write(command=>$head.$body);
+}
+
+sub threeDSwp_TimingSend {
+  my $self = shift;
+  my $command_name= "3dswp.timingsend";
+  my $bodysize = 0;
+  my $head= $self->nt_header($command_name,$bodysize,0);
+  $self->write(command=>$head);
+}
 =head1 Signals
 =cut
+
+
+
+
 
 sub Signals_MeasNamesGet {
   my $self = shift;
