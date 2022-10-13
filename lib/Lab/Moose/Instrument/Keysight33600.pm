b1fe28095d011319d3f231d8f2f1d33916164fb2
Simon Reinhardt <simon.reinhardt@ur.de>
2022-10-13 17:08:39 +0200

    Keysight 33500: Add USB vid and pid defaults

--- lib/Lab/Moose/Instrument/Keysight33600.pm
+++ lib/Lab/Moose/Instrument/Keysight33600.pm
@@ -18,6 +18,17 @@ use namespace::autoclean;
 
 extends 'Lab::Moose::Instrument';
 
+around default_connection_options => sub {
+    my $orig     = shift;
+    my $self     = shift;
+    my $options  = $self->$orig();
+    my $usb_opts = { vid => 0x0957, pid => 0x2b07 };
+    $options->{USB} = $usb_opts;
+    $options->{'VISA::USB'} = $usb_opts;
+    return $options;
+};
+
+
 sub BUILD {
     my $self = shift;
     $self->clear();
