2faef5ae9ff92bc4070adaf48e0142b73426866b
Deadmansshoe <robin.schock@posteo.de>
2025-04-08 10:16:21 +0200

    Updated the Cryomagnetics drivers to work with the Sweep::Magnet

--- lib/Lab/Moose/Instrument/Cryomagnetics_4G.pm
+++ lib/Lab/Moose/Instrument/Cryomagnetics_4G.pm
@@ -12,11 +12,36 @@ use Lab::Moose::Instrument qw/
 use Lab::Moose::Instrument::Cache;
 use Carp;
 use namespace::autoclean;
+use Lab::Moose::Countdown;
 
 extends 'Lab::Moose::Instrument';
 
 with qw(
     Lab::Moose::Instrument::Common
+);
+
+has verbose => (
+    is      => 'ro',
+    isa     => 'Bool',
+    default => 1
+);
+
+has heater_delay => (
+	is		=> 'ro',
+	isa		=> 'Lab::Moose::PosInt',
+	default => 60
+);
+
+has max_field_deviation => (
+	is		=> 'ro',
+	isa		=> 'Num',
+	default => 0.0001
+);
+
+has field_constant => (
+	is		=> 'ro',
+	isa		=> 'Num',
+	required => 1,
 );
 
 sub BUILD {
@@ -25,6 +50,8 @@ sub BUILD {
     if ( $units eq 'A' ) {
         carp
             'Warning: Cryomagnetics 4G Power supply uses current units (A) not field units';
+		carp 'Switch to Gauss (but everything will be given in Tesla) units!';
+		$self->set_units(value => 'G');
     }
 }
 
@@ -32,14 +59,20 @@ sub BUILD {
 
 =head1 SYNOPSIS
 
- use Lab::Moose;
+  use Lab::Moose;
+ 
+  # Constructor
+  my $magnet = instrument(
+      type => 'Cryomagnetics_4G',
+      connection_type => 'VISA::GPIB',
+      connection_options => {'gpib_address' => 10},
+	  field_constant => 18.7047 # A/T
+  );
 
- # Constructor
- my $magnet = instrument(
-     type => 'Cryomagnetics_4G',
-     connection_type => 'VISA::GPIB',
-     connection_options => {'gpib_address' => 10}
- );
+Eventhough the magnet power supply does support ethernet and USB besides GPIB, 
+both connectiontypes do not seem to work properly (on linux at least...).
+Ramp rates are internally set with a ramp current and therefore one needs to 
+supply the field constant of the magnet in units of A/T. 
  
 =cut
 
@@ -54,104 +87,355 @@ sub _kG_to_tesla {
     return 0.1 * $kG;
 }
 
+sub _tesla_to_kG {
+    my $tesla = shift;
+    return 10 * $tesla;
+}
+
+sub _tesla_to_amp {
+	my ($self, $value, %args) = validated_setter(
+		\@_,
+		value => { isa => 'num' },
+	);
+
+	return $value * field_constant;
+}
+	
+
 =head1 METHODS
+
+List of all high level methods. These are recommended to use for normal operation!
+
+=cut
+
+=head2 get_field
+
+  $magnet->get_field();
+
+Returns current powerline field in units of Tesla.
 
 =cut
 
 sub get_field {
     my $self = shift;
-    return _kg_to_tesla( $self->get_imag(@_) );
+    return _kG_to_tesla( $self->get_iout(@_) );
 }
 
-# =head1 sweep_to_field
+=head2 get_persistent_field
 
-#  my $new_field = $magnet->sweep_to_field(
-#     target => $target_field, # Tesla
-#     rate => $rate, # Tesla/min
-#  );
+  $magnet->get_persistent_field();
 
-# =cut
+Returns current persistent field in the magnet in units of Tesla.
 
-# sub sweep_to_field {
-#     my ( $self, %args ) = validated_getter(
-#         \@_,
-#         target => { isa => 'Num' },
-#         rate   => { isa => 'Num' },
-#     );
+=cut
 
-#     my $point = delete $args{target};
-#     my $rate  = delete $args{rate};
+sub get_persistent_field {
+    my $self = shift;
+    return _kG_to_tesla( $self->get_imag(@_) );
+}
 
-#     $self->config_sweep( point => $point, rate => $rate, %args );
+=head2 sweep_to_field
 
-#     $self->trg(%args);
+ my $new_field = $magnet->sweep_to_field(
+    target => $target_field, # Tesla
+    rate => $rate, # Tesla/min
+ );
 
-#     $self->wait(%args);
-#     return $self->get_field(%args);
-# }
+=cut
 
-# =head1 config_sweep
+sub sweep_to_field {
+    my ( $self, %args ) = validated_getter(
+        \@_,
+        target => { isa => 'Num' },
+        rate   => { isa => 'Num' },
+    );
 
-#  $magnet->config_sweep(point => $target, rate => $rate);
+    my $point = delete $args{target};
+    my $rate  = delete $args{rate};
 
-# Only define setpoints, do not start sweep.
+    $self->config_sweep( point => $point, rate => $rate, %args );
 
-# =cut
+    $self->trg(%args);
 
-# sub config_sweep {
-#     my ( $self, %args ) = validated_hash(
-#         \@_,
-#         point => { isa => 'Num' },
-#         rate  => { isa => 'Num' },
-#     );
-#     my $target = delete $args{point};
-#     my $rate   = delete $args{rate};
+    $self->wait(%args);
+    return $self->get_field(%args);
+}
 
-#     $self->set_field_sweep_rate( value => $rate, %args );
-#     $self->set_target_field( value => $target, %args );
+=head1 config_sweep
 
-#     my $current_field = $self->get_field();
-#     $self->_check_sweep_parameters(
-#         current => $current_field, target => $target,
-#         rate    => $rate
-#     );
-#     if ( $self->verbose() ) {
-#         say "config_sweep: target: $target (T), rate: $rate (T/min)";
-#     }
-# }
+ $magnet->config_sweep(point => $target, rate => $rate);
 
-# sub trg {
+Only define setpoints, do not start sweep.
 
-# }
+=cut
 
-# sub wait {
+sub config_sweep {
+    my ( $self, %args ) = validated_hash(
+        \@_,
+        point => { isa => 'Num' },
+        rate  => { isa => 'Num' },
+    );
+    my $target = delete $args{point};
+    my $rate   = delete $args{rate};
 
-# }
+	say "config sweep with target $target and rate $rate";
+
+	$self->set_field_sweep_rate( value => $rate);
+    $self->set_target_field( value => $target);
+
+    my $current_field = $self->get_field();
+    #$self->_check_sweep_parameters(
+    #    current => $current_field, target => $target,
+    #    rate    => $rate
+    #);
+    if ( $self->verbose() ) {
+        say "config_sweep: target: $target (T), rate: $rate (T/min)";
+    }
+}
+
+=head2 get/set_target_field
+
+  $magnet->set_target_field(value => 1);
+
+Sets or gets the next target field in Tesla.
+
+=cut
+
+sub set_target_field {
+	my ( $self, $value, %args) = validated_setter( 
+		\@_,
+		value => { isa => 'Num' },
+	);
+	$value = _tesla_to_kG($value);
+	$self->set_setpoint(value => $value);
+}
+
+sub get_target_field {
+	my ( $self, %args) = validated_getter( \@_ );
+	return _kG_to_tesla($self->get_setpoint());
+}
+
+=head2 set_field_sweep_rate
+
+  $magnet-set_sweep_rate(value => 1) # T/min
+
+Sets the sweep rate for all ranges to the given value in T/min.
+
+=cut
+
+sub set_field_sweep_rate {
+	my ( $self, $value, %args) = validated_setter( 
+		\@_,
+		value => { isa => 'Num' },
+	);
+	my $amprate = $self->_tesla_to_amp($value) / 60;
+	#my $gausrate = _tesla_to_kG($value) / 60;
+	for my $range (0 .. 4) {
+		$self->set_rate(range => $range, rate => $amprate);
+	};
+};
+
+
+sub to_setpoint {
+	my ( $self, %args) = validated_getter( \@_ );
+	if ($self->get_pshtr() == 0) {
+		$self->set_sweep( value => 'UP FAST' );
+	} else {
+		$self->set_sweep( value => 'UP SLOW' );
+	}
+}
+
+sub set_setpoint {
+	my ( $self, $value, %args) = validated_setter( 
+		\@_,
+		value => { isa => 'Num' },
+	);
+	my $lowerlimit = $self->get_llim();
+	# my upperlimit = $self->get_ulim();
+	if ($value < $lowerlimit) {
+		$self->set_llim(value => $value);
+		$self->set_ulim(value => $value);
+	} else {
+		$self->set_ulim(value => $value);
+		$self->set_llim(value => $value);
+	}
+}
+
+sub get_setpoint {
+	my ( $self, %args) = validated_getter( \@_ );
+	return $self->get_ulim( @_ );
+}
+
+sub trg {
+	my ( $self, %args) = validated_getter( \@_ );
+	$self->to_setpoint();	
+}
+
+sub wait {
+	my ( $self, %args) = validated_getter( \@_ );
+    my $target  = $self->get_target_field();
+    my $verbose = $self->verbose();
+    # enable autoflush
+    my $autoflush = STDOUT->autoflush();
+    my $last_field;
+    my $time_step = 1;
+    while (1) {
+        sleep $time_step;
+        my $field = $self->get_field(%args);
+        if ($verbose) {
+            my $rate;
+            if ( defined $last_field ) {
+                $rate = ( $field - $last_field ) * 60 / $time_step;
+                $rate = sprintf( "%.5g", $rate );
+            }
+            else {
+                $rate = "unknown";
+            }
+            printf(
+                "Field: %.6e T, Estimated rate: $rate T/min       \r",
+                $field
+            );
+            $last_field = $field;
+        }
+        if ( abs( $field - $target ) < $self->max_field_deviation() ) {
+			$self->hold();
+            last;
+        }
+    }
+    if ($verbose) {
+        print " " x 70 . "\r";
+    }
+    # reset autoflush to previous value
+    STDOUT->autoflush($autoflush);
+}
 
 # sub active {
 
 # }
 
-# sub in_persistent_mode {
+=head2 to_zero 
 
-# }
+  $magnet->to_zero();
 
-# sub get_persistent_field {
+Sweep magnet to 0 field.
 
-# }
+=cut 
 
-# =head2 heater_on/heater_off
+sub to_zero {
+	my ( $self, %args) = validated_getter( \@_ );
+	$self->set_sweep(value => 'ZERO', @_);
+}
 
-#  $magnet->heater_on();
-#  $magnet->heater_off();
+=head2 hold
 
-# sub heater_on {
+  $magnet->hold();
 
-# }
+Set magnet to hold and stop sweeping field.
 
-# sub heater_off {
+=cut
 
-# }
+sub hold {
+	my ( $self, %args) = validated_getter( \@_ );
+	$self->set_sweep(value => 'PAUSE', @_);
+}
+
+=head2 in_persistent_mode
+
+  if ($magnet->in_persistent_mode()) {
+    # go to desired filed
+  } else {
+	# Sweep up powerline field and start switch heater
+  };
+
+Check if magnet is in persistent mode (the switch heater is on).
+
+=cut
+
+sub in_persistent_mode {
+    my ( $self, %args ) = validated_getter( \@_ );
+	my $rv   = $self->get_pshtr(@_);
+    if ( $rv eq 1 ) {
+        return 0;
+    }
+    elsif ( $rv eq 0 ) {
+        return 1;
+    }
+    else {
+        croak("unknown heater setting $rv");
+    }
+}
+
+=head2 unset_persistent_mode 
+
+  $magnet->unset_persistent_mode()
+
+Disables the persistent mode by driving up powerline field and starting switch heater
+
+=cut
+
+sub unset_persistent_mode {
+	my ( $self, %args ) = validated_getter( \@_ );
+	if ($self->in_persistent_mode()) {
+		my $pers_field = $self->get_persistent_field();
+		my $powerline_field = $self->get_field();
+
+		if (abs($powerline_field - $pers_field) < $self->max_field_deviation()) {
+			$self->heater_on();
+			return;
+		} else {
+			$self->set_target_field( value => $pers_field );
+			$self->set_sweep(value => 'UP FAST');
+			countdown(120/5);
+			$self->set_sweep(value => 'UP');
+			#$self->wait();
+			$self->heater_on();
+			return;
+		};
+	} else {
+		return;
+	};
+}
+
+=head2 set_persistent_mode 
+
+  $magnet->set_persistent_mode()
+
+Enables the persistent mode with the current field.
+
+=cut
+
+sub set_persistent_mode {
+	my ( $self, %args) = validated_getter( \@_ );
+	if ($self->in_persistent_mode()) {
+		return;
+	} else {
+		$self->heater_off();
+		$self->to_zero();
+		# $self->wait();
+		return;
+	};
+}
+
+=head2 heater_on/heater_off
+
+  $magnet->heater_on();
+  $magnet->heater_off();
+
+Set the switch heater to be active or not active. Waits to let it warm up.
+
+=cut
+
+sub heater_on {
+	my ( $self, %args) = validated_getter( \@_ );
+	$self->set_pshtr(value => 'On', @_);
+	countdown( $self->heater_delay(), "Activated switch heater. Waiting for: ");
+}
+
+sub heater_off {
+	my ( $self, %args) = validated_getter( \@_ );
+	$self->set_pshtr(value => 'Off', @_);
+	countdown( $self->heater_delay(), "Deactivated switch heater. Waiting for: ");
+}
 
 =head1 LOW-LEVEL DEVICE METHODS
 
@@ -165,7 +449,8 @@ Uses either Amps or kilo Gauss, depending on units setting.
 
 sub get_imag {
     my ( $self, %args ) = validated_getter( \@_ );
-    return $self->query( command => "IMAG?", %args );
+    my $rv = $self->query( command => "IMAG?", %args );
+	return _remove_unit($rv);
 }
 
 =head2 get_iout
@@ -238,7 +523,7 @@ Returns 0 or 1.
 sub set_pshtr {
     my ( $self, $value, %args ) = validated_setter(
         \@_,
-        value => { isa => enum( [ 0, 1 ] ) },
+        value => { isa => enum( [qw/On Off/] ) },
     );
     $self->write( command => "PSHTR $value", %args );
 }
@@ -259,6 +544,7 @@ C<select> is in range 0,..,4.
 
 sub set_range {
     my ( $self, %args ) = validated_getter(
+        \@_,
         select => { isa => enum( [ ( 0 .. 4 ) ] ) },
         limit => { isa => 'Num' },
     );
@@ -269,6 +555,7 @@ sub set_range {
 
 sub get_range {
     my ( $self, %args ) = validated_getter(
+        \@_,
         select => { isa => enum( [ ( 0 .. 4 ) ] ) },
     );
     my $select = delete $args{select};
@@ -287,6 +574,7 @@ C<rate> arg is given in Amps per second.
 
 sub set_rate {
     my ( $self, %args ) = validated_getter(
+        \@_,
         range => { isa => enum( [ ( 0 .. 5 ) ] ) },
         rate => { isa => 'Num' },
     );
@@ -297,6 +585,7 @@ sub set_rate {
 
 sub get_rate {
     my ( $self, %args ) = validated_getter(
+        \@_,
         range => { isa => enum( [ ( 0 .. 5 ) ] ) },
     );
     my $range = delete $args{range};
@@ -318,7 +607,7 @@ sub set_sweep {
         \@_,
         value => {
             isa => enum(
-                [ qw/UP DOWN PAUSE ZERO LIMIT/, 'UP FAST', 'DOWN FAST' ]
+                [ qw/UP DOWN PAUSE ZERO LIMIT/, 'UP SLOW', 'DOWN SLOW', 'UP FAST', 'DOWN FAST' ]
             )
         },
     );
